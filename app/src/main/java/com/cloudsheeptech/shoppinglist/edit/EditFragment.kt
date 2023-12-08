699b73f09398e6bec137293342dfef7c8fb281a5
Justus <beek@in.tum.de>
2023-12-08 14:52:27 +0100

    Added edit item background structure

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/edit/EditFragment.kt
@@ -0,0 +1,58 @@
+package com.cloudsheeptech.shoppinglist.edit
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.cloudsheeptech.shoppinglist.R
+import com.cloudsheeptech.shoppinglist.databinding.FragmentEditBinding
+import java.io.File
+
+class EditFragment : Fragment() {
+
+    private lateinit var binding : FragmentEditBinding
+    private lateinit var viewModel : EditViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit, container, false)
+
+//        val vocabFile = File(requireActivity().applicationContext.filesDir, "vocabulary.json")
+//        val vocabulary = Vocabulary.getInstance(vocabFile)
+        val viewModelFactory = EditViewModelFactory()
+        viewModel = ViewModelProvider(requireActivity(), viewModelFactory)[EditViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        try {
+            val navigationArgs = EditFragmentArgs.fromBundle(requireArguments())
+            viewModel.loadWord(navigationArgs.selectedId)
+        } catch (ex : IllegalArgumentException) {
+            Log.i("EditFragment", "Failed to load navargs!")
+            viewModel.loadWord(0)
+        }
+
+        viewModel.navigateUp.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                viewModel.wordEdited()
+                findNavController().navigateUp()
+            }
+        })
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/edit/EditViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.cloudsheeptech.shoppinglist.edit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class EditViewModelFactory() : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(EditViewModel::class.java)) {
+            return EditViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown class")
+    }
+}

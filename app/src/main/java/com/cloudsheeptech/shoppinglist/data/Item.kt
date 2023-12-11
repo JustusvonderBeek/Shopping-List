74fbd2ee350bb9cc2c965c6972fe722d484eaa2b
Justus von der Beek <justusvonderbeek@gmail.com>
2023-12-11 18:44:47 +0100

    Updated item to compareable

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/Item.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/Item.kt
@@ -1,7 +1,18 @@
 package com.cloudsheeptech.shoppinglist.data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Item(
-    var ID : Long,
-    var Name : String,
-    var ImagePath : String
-)
+    var ID: Long,
+    var Name: String,
+    var ImagePath: String
+) : Comparable<Item> {
+    override fun compareTo(other: Item): Int {
+        if (other.ID == this.ID)
+            return 0
+        if (other.ID > this.ID)
+            return -1
+        return 1
+    }
+}

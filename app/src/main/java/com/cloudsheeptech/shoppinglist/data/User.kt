5a4a6772be9ec44c24901a00a8cd040843cc0283
Justus von der Beek <justusvonderbeek@gmail.com>
2023-12-11 18:46:03 +0100

    Added default init values

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/User.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/User.kt
@@ -1,10 +1,12 @@
 package com.cloudsheeptech.shoppinglist.data
 
+import androidx.room.Entity
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Entity
 data class User(
-    var ID : Long,
-    var Name : String,
-    var FavouriteRecipe : Long
+    var ID : Long = 0L,
+    var Name : String = "",
+    var FavouriteRecipe : Long = -1L
 )

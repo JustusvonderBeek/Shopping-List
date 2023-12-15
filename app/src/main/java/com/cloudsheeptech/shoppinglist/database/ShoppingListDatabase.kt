6bca5361a94891d259eb5dd87a3caceb3dda854b
Justus <beek@in.tum.de>
2023-12-15 18:52:57 +0100

    Added room database for items and shopping list mappings

--- app/src/main/java/com/cloudsheeptech/shoppinglist/database/ShoppingListDatabase.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/database/ShoppingListDatabase.kt
@@ -6,14 +6,17 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.cloudsheeptech.shoppinglist.data.Item
+import com.cloudsheeptech.shoppinglist.data.ListMapping
 import com.cloudsheeptech.shoppinglist.data.ShoppingList
-import com.cloudsheeptech.shoppinglist.data.User
 
-@Database(entities = [ShoppingList::class], version=1, exportSchema = false)
+@Database(entities = [ShoppingList::class, Item::class, ListMapping::class], version=7, exportSchema = false)
 @TypeConverters(value = [DatabaseTypeConverter::class])
 abstract class ShoppingListDatabase : RoomDatabase() {
 
     abstract fun shoppingListDao() : ShoppingListDao
+    abstract fun itemListDao() : ItemListDao
+    abstract fun mappingDao() : ItemListMappingDao
 
     companion object {
         @Volatile

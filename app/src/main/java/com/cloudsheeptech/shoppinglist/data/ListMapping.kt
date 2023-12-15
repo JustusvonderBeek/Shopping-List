4ec693a685f382c25855265c66fe6df9c43a879d
Justus <beek@in.tum.de>
2023-12-15 18:53:16 +0100

    Added room item mapping

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ListMapping.kt
@@ -0,0 +1,15 @@
+package com.cloudsheeptech.shoppinglist.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Entity(tableName = "item_to_list_mapping")
+data class ListMapping(
+    @PrimaryKey(autoGenerate = true)
+    var ID : Long,
+    var ItemID : Long,
+    var ListID : Long,
+    var Quantity : Long,
+)
---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/database/ItemListMappingDao.kt
@@ -0,0 +1,39 @@
+package com.cloudsheeptech.shoppinglist.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.cloudsheeptech.shoppinglist.data.ListMapping
+
+@Dao
+interface ItemListMappingDao {
+
+    @Insert
+    fun insertMapping(listMapping: ListMapping)
+
+    @Update
+    fun updateMapping(listMapping: ListMapping)
+
+    @Query("DELETE FROM item_to_list_mapping WHERE ID = :key")
+    fun deleteMapping(key : Long)
+
+    @Query("DELETE FROM item_to_list_mapping WHERE ItemID = :itemId AND ListID = :listId")
+    fun deleteMappingItemListId(itemId : Long, listId : Long)
+
+    @Query("DELETE FROM item_to_list_mapping")
+    fun clearAll()
+
+    @Query("SELECT * FROM item_to_list_mapping WHERE ListID = :listId")
+    fun getMappingsForList(listId : Long) : List<ListMapping>
+
+    @Query("SELECT * FROM item_to_list_mapping WHERE ListID = :listId")
+    fun getMappingsForListLive(listId : Long) : LiveData<List<ListMapping>>
+
+    @Query("SELECT * FROM item_to_list_mapping WHERE ItemID = :itemId")
+    fun getMappingsForItem(itemId : Long) : List<ListMapping>
+
+    @Query("SELECT * FROM item_to_list_mapping WHERE ItemID = :itemId")
+    fun getMappingsForItemLive(itemId : Long) : LiveData<List<ListMapping>>
+}

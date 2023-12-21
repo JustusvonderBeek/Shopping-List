a65c04417c349fcdf21d6fcc68f52cfd57cb3926
Justus <beek@in.tum.de>
2023-12-21 16:43:01 +0100

    Include checked in data model

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/ItemWithQuantity.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ItemWithQuantity.kt
@@ -1,10 +1,9 @@
 package com.cloudsheeptech.shoppinglist.data
 
-import androidx.room.PrimaryKey
-
 data class ItemWithQuantity(
     var ID: Long,
     var Name: String,
     var ImagePath: String,
     var Quantity : Long,
+    var Checked : Boolean,
 )

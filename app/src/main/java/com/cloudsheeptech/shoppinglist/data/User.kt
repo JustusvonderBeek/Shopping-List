71080b5923b7de59be4ddbfe336510e1fe5a62d3
Justus <beek@in.tum.de>
2024-01-05 15:43:10 +0100

    Started migration of user to database

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/User.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/User.kt
@@ -1,11 +1,13 @@
 package com.cloudsheeptech.shoppinglist.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Entity
+@Entity(tableName = "user_table")
 data class User(
+    @PrimaryKey(autoGenerate = false)
     var ID : Long = 0L,
     var Username : String = "",
     var Password : String = ""

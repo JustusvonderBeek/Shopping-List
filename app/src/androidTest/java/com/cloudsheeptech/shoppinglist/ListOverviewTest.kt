84dfc9b70e9b6a8be1c9698fef96cf5eaebaa5fe
Justus <beek@in.tum.de>
2024-01-07 19:38:13 +0100

    Reset items and lists

---
+++ app/src/androidTest/java/com/cloudsheeptech/shoppinglist/ListOverviewTest.kt
@@ -0,0 +1,20 @@
+package com.cloudsheeptech.shoppinglist
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ListOverviewTest {
+
+    @Test
+    fun testLoadingUserNoExisting() {
+
+    }
+
+    @Test
+    fun testLoadingUserExists() {
+
+    }
+
+}

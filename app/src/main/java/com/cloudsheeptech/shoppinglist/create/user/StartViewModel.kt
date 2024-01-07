4f7bb1ff5c035a5034e556648c4dc2a738493939
Justus <beek@in.tum.de>
2024-01-07 15:29:52 +0100

    Refactored

--- app/src/main/java/com/cloudsheeptech/shoppinglist/create/user/StartViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/create/user/StartViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.cloudsheeptech.shoppinglist.data.User
+import com.cloudsheeptech.shoppinglist.database.ShoppingListDatabase
 import com.cloudsheeptech.shoppinglist.network.Networking
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
@@ -28,26 +29,35 @@ class StartViewModel(application: Application) : AndroidViewModel(application) {
     private val job = Job()
     private val recapScope = CoroutineScope(Dispatchers.IO + job)
 
-    private val letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?$%&(){}[]"
-    private val defaultPasswordLength = 32
+    // -----------------------------------------------
 
-    val inputText = MutableLiveData<String>()
+    private val database = ShoppingListDatabase.getInstance(application.applicationContext)
+    private val userDao = database.userDao()
 
-    private val _navigateToApp = MutableLiveData<Boolean>(false)
-    val navigateToApp : LiveData<Boolean> get() = _navigateToApp
+    // UI State
 
     private val _hideKeyboard = MutableLiveData<Boolean>(false)
     val hideKeyboard : LiveData<Boolean> get() = _hideKeyboard
 
+    // Data
+
+    private val letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?$%&(){}[]"
+    private val defaultPasswordLength = 32
     private val jsonSerializer = Json {
         ignoreUnknownKeys = false
         encodeDefaults = true
     }
 
+    val user = userDao.getUserLive()
+    val inputText = MutableLiveData<String>()
+
+
+    // -----------------------------------------------
+
     init {
-        val exists = checkUserExists()
-        if (exists)
-            navigateToApp()
+        if (user.value != null) {
+            Log.i("StartViewModel", "User already exists. Navigate back")
+        }
     }
 
     private fun generatePassword() : String {
@@ -60,63 +70,23 @@ class StartViewModel(application: Application) : AndroidViewModel(application) {
         return password.toString()
     }
 
-    private suspend fun loadUser() : User? {
-        var user: User? = null
+    private suspend fun storeUser(user : User) {
         withContext(Dispatchers.IO) {
             try {
-                val file = File(getApplication<Application>().filesDir, "user.json")
-                if (file.exists())
-                    return@withContext
-                val reader = file.reader(Charsets.UTF_8)
-                val content = reader.readText()
-                user = jsonSerializer.decodeFromString<User>(content)
+                userDao.resetUser()
+                // Checking in the fragment if the user is found and navigate in this case
+                userDao.insertUser(user)
+                Log.i("StartViewModel", "Stored user to database")
             } catch (ex : Exception) {
-                Log.i("StartViewModel", "No user found. Creating one")
-            }
-        }
-        return user
-    }
-
-    private fun checkUserExists() : Boolean {
-        var exists = false
-        recapScope.launch {
-            val user = loadUser()
-            if (user != null)
-                exists = true
-        }
-        return exists
-    }
-
-    private suspend fun storeUser(response: String, password : String) {
-        withContext(Dispatchers.IO) {
-            try {
-                // Store username and ID to disk
-                val decodedUser = Json.decodeFromString<User>(response)
-                if (decodedUser.ID == 0L || decodedUser.Password != "accepted") {
-                    Log.w("StartViewModel", "User not correctly initialized. Storing for offline use")
-//                    return@withContext false
-                }
-                decodedUser.Password = password
-                val encodedUser = jsonSerializer.encodeToString(decodedUser)
-                val file = File(getApplication<Application>().filesDir, "user.json")
-                val writer = file.writer(Charsets.UTF_8)
-                writer.write(encodedUser)
-                writer.close()
-                Log.i("StartViewModel", "Stored user $encodedUser to disk")
-                return@withContext true
-            } catch (ex : Exception) {
-                Log.w("StartViewModel", "Failed to save username to disk: $ex")
+                Log.w("StartViewModel", "Failed to save user: $ex")
                 // We don't want to proceed in case the user cannot be stored!
                 Toast.makeText(getApplication(), "Failed to store user!", Toast.LENGTH_SHORT).show()
                 throw ex
             }
         }
-        withContext(Dispatchers.Main) {
-            navigateToApp()
-        }
     }
 
-    private suspend fun pushUsernameToServer(user : User) {
+    private suspend fun pushUsernameToServer(user : User) : User {
         withContext(Dispatchers.IO) {
             val serializedUser = jsonSerializer.encodeToString(user)
             // Make sure that one can use the app without internet
@@ -127,15 +97,21 @@ class StartViewModel(application: Application) : AndroidViewModel(application) {
                     withContext(Dispatchers.Main) {
                         Toast.makeText(getApplication(), "Failed to store user at server", Toast.LENGTH_SHORT).show()
                     }
-                    storeUser(serializedUser, user.Password)
                     return@POST
                 }
                 withContext(Dispatchers.Main) {
                     Toast.makeText(getApplication(), "Created new user online", Toast.LENGTH_SHORT).show()
                 }
-                storeUser(response.bodyAsText(Charsets.UTF_8), user.Password)
+                val respBody = response.bodyAsText(Charsets.UTF_8)
+                val decodedUser = Json.decodeFromString<User>(respBody)
+                if (decodedUser.ID == 0L || decodedUser.Password != "accepted") {
+                    Log.e("StartViewModel", "User not correctly initialized!")
+                    return@POST
+                }
+                user.ID = decodedUser.ID
             }
         }
+        return user
     }
 
     fun pushUsername() {
@@ -148,9 +124,12 @@ class StartViewModel(application: Application) : AndroidViewModel(application) {
         val user = User(0, inputText.value!!, password)
         Log.d("StartViewModel", "Creating user $user")
         recapScope.launch {
-            pushUsernameToServer(user)
+            val onlineUser = pushUsernameToServer(user)
+            storeUser(onlineUser)
         }
     }
+
+    // -----------------------------------------------
 
     private fun hideKeyboard() {
         _hideKeyboard.value = true
@@ -158,13 +137,5 @@ class StartViewModel(application: Application) : AndroidViewModel(application) {
 
     fun keyboardHidden() {
         _hideKeyboard.value = false
-    }
-
-    fun navigateToApp() {
-        _navigateToApp.value = true
-    }
-
-    fun onAppNavigated() {
-        _navigateToApp.value = false
     }
 }

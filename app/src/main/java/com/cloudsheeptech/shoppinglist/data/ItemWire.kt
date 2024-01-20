df295f5074cdaf8c68698e733f5a41a853d4c4fc
Justus <beek@in.tum.de>
2024-01-20 20:32:16 +0100

    Added struct to send infos wire

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ItemWire.kt
@@ -0,0 +1,11 @@
+package com.cloudsheeptech.shoppinglist.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ItemWire(
+    var Name: String,
+    var Icon: String,
+    var Quantity : Long,
+    var Checked : Boolean,
+)

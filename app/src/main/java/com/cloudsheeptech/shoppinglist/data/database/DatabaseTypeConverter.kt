a4c284ce151af3d15495402f20a32ba96960a244
Justus von der Beek <justusvonderbeek@gmail.com>
2024-01-27 19:00:14 +0100

    Include list creator type converter

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/DatabaseTypeConverter.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/DatabaseTypeConverter.kt
@@ -1,6 +1,7 @@
 package com.cloudsheeptech.shoppinglist.data.database
 
 import androidx.room.TypeConverter
+import com.cloudsheeptech.shoppinglist.data.ListCreator
 import com.cloudsheeptech.shoppinglist.data.User
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -15,6 +16,16 @@ class DatabaseTypeConverter {
     @TypeConverter
     fun stringToUser(string : String) : User {
         return Json.decodeFromString<User>(string)
+    }
+
+    @TypeConverter
+    fun listCreatorToString(listCreator: ListCreator) : String {
+        return Json.encodeToString(listCreator)
+    }
+
+    @TypeConverter
+    fun stringToListCreator(string: String) : ListCreator {
+        return Json.decodeFromString(string)
     }
 
 }

e022595e9442756d7a91739f165885498a404de7
Justus <beek@in.tum.de>
2024-01-27 09:52:55 +0100

    Testing for list operations

--- app/src/androidTest/java/com/cloudsheeptech/shoppinglist/ListOperationTests.kt
+++ app/src/androidTest/java/com/cloudsheeptech/shoppinglist/ListOperationTests.kt
@@ -2,16 +2,14 @@ package com.cloudsheeptech.shoppinglist
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStore
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.cloudsheeptech.shoppinglist.fragments.create.list.CreateShoppinglistViewModel
-import com.cloudsheeptech.shoppinglist.fragments.create.list.CreateShoppinglistViewModelFactory
+import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
+import com.cloudsheeptech.shoppinglist.data.handling.ShoppingListHandler
+import com.cloudsheeptech.shoppinglist.network.Networking
 import com.cloudsheeptech.shoppinglist.user.AppUser
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.withContext
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -19,18 +17,35 @@ import org.junit.runner.RunWith
 class ListOperationTests {
 
     private suspend fun createList() : Boolean {
-        val listTitle = "New List"
+//        val listTitle = "New List"
         val application = ApplicationProvider.getApplicationContext<Application>()
         AppUser.loadUser(application)
-        val factory = CreateShoppinglistViewModelFactory(application)
-        val store = ViewModelStore()
-        val viewModel = ViewModelProvider(store, factory)[CreateShoppinglistViewModel::class.java]
-        // Setting the required properties
-        withContext(Dispatchers.Main) {
-            viewModel.title.value = listTitle
-        }
-        viewModel.create()
-        Thread.sleep(1000)
+        AppUser.ID = 12
+        AppUser.Username = "Franz"
+
+//        val factory = CreateShoppinglistViewModelFactory(application)
+//        val store = ViewModelStore()
+//        val viewModel = ViewModelProvider(store, factory)[CreateShoppinglistViewModel::class.java]
+//        // Setting the required properties
+//        withContext(Dispatchers.Main) {
+//            viewModel.title.value = listTitle
+//        }
+//        viewModel.create()
+        val database = ShoppingListDatabase.getInstance(application)
+        Networking.registerApplicationDir(application.filesDir.absolutePath, database)
+        val listHandler = ShoppingListHandler(database = database)
+        listHandler.CreateNewShoppingList("Neue Liste")
+        Thread.sleep(500)
+        listHandler.CreateNewShoppingList("Zweite Liste")
+        Thread.sleep(500)
+
+        // Expecting two lists to be created
+        val itemDao = database.itemListDao()
+        val allItems = itemDao.getAllItems()
+        Assert.assertEquals(0, allItems.size)
+        val listDao = database.shoppingListDao()
+        val allLists = listDao.getShoppingLists()
+        Assert.assertEquals(2, allLists.size)
         return true
     }
 
@@ -49,8 +64,22 @@ class ListOperationTests {
 
     }
 
-    fun testLoadList() {
+    private suspend fun loadList() : Boolean {
+        val application = ApplicationProvider.getApplicationContext<Application>()
+        AppUser.loadUser(application)
+        AppUser.ID = 12
+        AppUser.Username = "Franz"
+        val database = ShoppingListDatabase.getInstance(application)
+        Networking.registerApplicationDir(application.filesDir.absolutePath, database)
+        val listHandler = ShoppingListHandler(database = database)
+        listHandler.GetShoppingList(12L)
+        return true
+    }
 
+    fun testLoadList() = runTest {
+        Log.i("ListOperationTest", "Testing obtaining list from online")
+        val success = loadList()
+        assert(success)
     }
 
     fun testAddItemToList() {

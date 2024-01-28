09d815d4c8eb151f6096c506e39d85853f57aac0
Justus von der Beek <justusvonderbeek@gmail.com>
2024-01-28 15:08:31 +0100

    Removed unused

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListViewModelFactory.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListViewModelFactory.kt
@@ -7,10 +7,10 @@ import com.cloudsheeptech.shoppinglist.data.ShoppingList
 import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
 import com.cloudsheeptech.shoppinglist.datastructures.ItemListWithName
 
-class ShoppingListViewModelFactory(val itemListWithName: ItemListWithName<Item>, val database : ShoppingListDatabase, val shoppingListId : Long) : ViewModelProvider.Factory {
+class ShoppingListViewModelFactory(val database : ShoppingListDatabase, val shoppingListId : Long) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ShoppinglistViewModel::class.java)) {
-            return ShoppinglistViewModel(itemListWithName, database, shoppingListId) as T
+            return ShoppinglistViewModel(database, shoppingListId) as T
         }
         throw IllegalArgumentException("Unknown class")
     }

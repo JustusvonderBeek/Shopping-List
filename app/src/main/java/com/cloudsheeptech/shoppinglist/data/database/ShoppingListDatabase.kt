2b3332ccd0b113f4fae249eb4efe66ff3f0d82e1
Justus von der Beek <justusvonderbeek@gmail.com>
2024-01-28 15:07:49 +0100

    Renamed

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/ShoppingListDatabase.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/ShoppingListDatabase.kt
@@ -11,12 +11,12 @@ import com.cloudsheeptech.shoppinglist.data.ListMapping
 import com.cloudsheeptech.shoppinglist.data.ShoppingList
 import com.cloudsheeptech.shoppinglist.data.User
 
-@Database(entities = [ShoppingList::class, Item::class, ListMapping::class, User::class], version=9, exportSchema = false)
+@Database(entities = [ShoppingList::class, Item::class, ListMapping::class, User::class], version=10, exportSchema = false)
 @TypeConverters(value = [DatabaseTypeConverter::class])
 abstract class ShoppingListDatabase : RoomDatabase() {
 
     abstract fun shoppingListDao() : ShoppingListDao
-    abstract fun itemListDao() : ItemListDao
+    abstract fun itemDao() : ItemDao
     abstract fun mappingDao() : ItemListMappingDao
     abstract fun userDao() : UserDao
 

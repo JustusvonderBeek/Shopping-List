c0d9e306d4002b824b74624a4fa24b0eab9d588e
Justus <beek@in.tum.de>
2024-02-07 20:01:13 +0100

    Started combining offline and online users

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/share/ShareViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/share/ShareViewModel.kt
@@ -2,9 +2,13 @@ package com.cloudsheeptech.shoppinglist.fragments.share
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cloudsheeptech.shoppinglist.data.ListCreator
+import com.cloudsheeptech.shoppinglist.data.ListShare
+import com.cloudsheeptech.shoppinglist.data.ListShareDatabase
+import com.cloudsheeptech.shoppinglist.data.ShareUserPreview
 import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
 import com.cloudsheeptech.shoppinglist.data.handling.ShoppingListHandler
 import kotlinx.coroutines.CoroutineScope
@@ -17,30 +21,74 @@ class ShareViewModel(val database : ShoppingListDatabase, private val listId : L
 
     private val job = Job()
     private val localCoroutine = CoroutineScope(Dispatchers.Main + job)
+    private val sharedDao = database.sharedDao()
+    private val onlineUserDao = database.onlineUserDao()
 
     private val listHandler = ShoppingListHandler(database)
 
     val searchName = MutableLiveData<String>("")
-    private val _searchedUsers = MutableLiveData<List<ListCreator>>()
-    val searchedUsers : LiveData<List<ListCreator>> get() = _searchedUsers
+    private val _searchedUsers = MutableLiveData<List<ShareUserPreview>>()
+    val searchedUsers : LiveData<List<ShareUserPreview>> get() = _searchedUsers
+
+    private var _offlineShared = sharedDao.getListSharedWithLive(listId)
+
+    private val _shared = MediatorLiveData<List<ShareUserPreview>>()
+    val sharedPreview : LiveData<List<ShareUserPreview>> get() = _shared
+
+    // --- Navigation / UI States ---
 
     private val _navigateUp = MutableLiveData<Boolean>(false)
     val navigateUp : LiveData<Boolean> get() = _navigateUp
 
     init {
         _searchedUsers.value = emptyList()
+        initPreview()
     }
 
-    private suspend fun searchUsersFromOnlineAndDatabase(name : String) : List<ListCreator> {
-        var users = emptyList<ListCreator>()
+    private fun initPreview() {
+        _shared.addSource(_offlineShared) { offlinePreview ->
+            Log.d("ShareViewModel", "Offline changed...")
+            localCoroutine.launch {
+                val converted = convertListShareToPreviewUser(offlinePreview)
+                combineWithOnlineUser(converted)
+            }
+        }
+    }
+
+    private suspend fun convertListShareToPreviewUser(share : List<ListShareDatabase>) : List<ShareUserPreview> {
+        val previewUsers = mutableListOf<ShareUserPreview>()
+        withContext(Dispatchers.IO) {
+            share.forEach { s ->
+                val user = onlineUserDao.getUser(s.SharedWith)
+                if (user != null) {
+                    previewUsers.add(ShareUserPreview(user.ID, user.Username, true))
+                }
+            }
+        }
+        return previewUsers
+    }
+
+    private fun combineWithOnlineUser(offlinePreview : List<ShareUserPreview>) {
+        val combinedShared = mutableListOf<ShareUserPreview>()
+        combinedShared.addAll(offlinePreview)
+        _shared.addSource(_searchedUsers) { onlineUsers ->
+            val onlinePreview = onlineUsers.map { x -> ShareUserPreview(x.UserId, x.Name, false) }
+            combinedShared.addAll(onlinePreview)
+            _shared.value = combinedShared
+        }
+    }
+
+    private suspend fun searchUsersFromOnlineAndDatabase(name : String) : List<ShareUserPreview> {
+        var users = emptyList<ShareUserPreview>()
         withContext(Dispatchers.IO) {
             val onlineUsers = listHandler.SearchUsersOnline(name) ?: return@withContext
-            users = onlineUsers
+            val onlinePreview = onlineUsers.map { x -> ShareUserPreview(x.ID, x.Name, false) }
+            users = onlinePreview
         }
         return users
     }
 
-    private suspend fun updateListCreators(list : List<ListCreator>) {
+    private suspend fun updateListCreators(list : List<ShareUserPreview>) {
         withContext(Dispatchers.Main) {
             _searchedUsers.value = list
         }
@@ -65,6 +113,10 @@ class ShareViewModel(val database : ShoppingListDatabase, private val listId : L
 
     fun unshareList() {
         listHandler.UnshareShoppingListOnline(listId)
+    }
+
+    fun unshareListForUser(userId : Long) {
+        listHandler.UnshareShoppingListForUserOnline(userId, listId)
     }
 
     fun navigateUp() {

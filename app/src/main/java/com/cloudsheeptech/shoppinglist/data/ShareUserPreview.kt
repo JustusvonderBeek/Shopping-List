bdbd4e9383ff8babc6b111f2f2cd29ed94993108
Justus <beek@in.tum.de>
2024-02-09 12:41:20 +0100

    Added comparison for list

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/ShareUserPreview.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ShareUserPreview.kt
@@ -4,4 +4,11 @@ data class ShareUserPreview(
     var UserId : Long,
     var Name : String,
     var Shared : Boolean
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other is ShareUserPreview) {
+            return other.UserId == this.UserId
+        }
+        return false
+    }
+}

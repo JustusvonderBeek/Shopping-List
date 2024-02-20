03ac9447d80e60c29cebf056e42e5931e49bcef3
Justus <beek@in.tum.de>
2024-02-20 14:56:23 +0100

    Removed unused

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/OnlineUserDao.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/OnlineUserDao.kt
@@ -1,13 +1,11 @@
 package com.cloudsheeptech.shoppinglist.data.database
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.cloudsheeptech.shoppinglist.data.User
 import com.cloudsheeptech.shoppinglist.data.UserWire
 
 @Dao

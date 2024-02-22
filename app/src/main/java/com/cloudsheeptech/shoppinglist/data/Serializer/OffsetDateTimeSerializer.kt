460f5220a320547ef6b198eb3a3d6ef478d90de3
Justus <beek@in.tum.de>
2024-02-22 15:51:02 +0100

    Added offsetdatetime serialization

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/Serializer/OffsetDateTimeSerializer.kt
@@ -0,0 +1,25 @@
+package com.cloudsheeptech.shoppinglist.data.Serializer
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = OffsetDateTime::class)
+class OffsetDateTimeSerializer : KSerializer<OffsetDateTime> {
+
+    private val formatter : DateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
+    override fun serialize(encoder: Encoder, value: OffsetDateTime) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    override fun deserialize(decoder: Decoder): OffsetDateTime {
+        return OffsetDateTime.parse(decoder.decodeString(), formatter)
+    }
+
+}
--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/DatabaseTypeConverter.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/DatabaseTypeConverter.kt
@@ -3,10 +3,26 @@ package com.cloudsheeptech.shoppinglist.data.database
 import androidx.room.TypeConverter
 import com.cloudsheeptech.shoppinglist.data.ListCreator
 import com.cloudsheeptech.shoppinglist.data.DatabaseUser
+import com.cloudsheeptech.shoppinglist.data.Serializer.OffsetDateTimeSerializer
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Base64.Encoder
 
 class DatabaseTypeConverter {
+
+    @TypeConverter
+    fun offsetDateToString(date : OffsetDateTime) : String {
+        val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+        return date.format(formatter)
+    }
+
+    @TypeConverter
+    fun stringToOffsetDate(date : String) : OffsetDateTime {
+        val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+        return OffsetDateTime.parse(date, formatter)
+    }
 
     @TypeConverter
     fun userToString(user : DatabaseUser) : String {

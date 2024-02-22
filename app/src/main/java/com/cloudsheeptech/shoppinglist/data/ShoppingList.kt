d487844292524e6df65a65b40c7752b14a215b86
Justus <beek@in.tum.de>
2024-02-22 15:54:47 +0100

    Updated with createdBy and Creation Time

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/ShoppingList.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ShoppingList.kt
@@ -3,12 +3,15 @@ package com.cloudsheeptech.shoppinglist.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
+import java.time.OffsetDateTime
 
-@Entity(tableName = "list_table")
+@Entity(tableName = "list_table", primaryKeys = ["ID", "CreatedBy"])
 data class ShoppingList(
-    @PrimaryKey(autoGenerate = true)
+//    @PrimaryKey(autoGenerate = true)
     var ID : Long,
     var Name : String,
-    var CreatedBy : ListCreator,
-    var LastEdited : String
+    // Flatten list creator to allow direct ID access
+    var CreatedBy : Long,
+    var CreatedByName : String,
+    var LastEdited : OffsetDateTime
 )

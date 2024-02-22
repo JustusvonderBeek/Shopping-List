5e239e07b78d691952b90cde69243df4b0e120bd
Justus <beek@in.tum.de>
2024-02-22 15:56:25 +0100

    Added created and modified edited data type

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/ShoppingListWire.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ShoppingListWire.kt
@@ -1,12 +1,17 @@
 package com.cloudsheeptech.shoppinglist.data
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.time.OffsetDateTime
 
 @Serializable
 data class ShoppingListWire(
     var ListId : Long,
     var Name : String,
     var CreatedBy : ListCreator,
-    var LastEdited : String,
+    @Contextual
+    var Created : OffsetDateTime,
+    @Contextual
+    var LastEdited : OffsetDateTime,
     var Items : MutableList<ItemWire>
 )

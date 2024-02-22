8d4f6fec4639248f092d9d0162dc757718d2fc1b
Justus <beek@in.tum.de>
2024-02-22 15:53:54 +0100

    Updated and included createdBy and created

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/ListShare.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/ListShare.kt
@@ -1,9 +1,14 @@
 package com.cloudsheeptech.shoppinglist.data
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.time.OffsetDateTime
 
 @Serializable
 data class ListShare(
     var ListId : Long,
-    var SharedWith : Long
+    var CreatedBy : Long,
+    var SharedWith : Long,
+    @Contextual
+    var Created : OffsetDateTime,
 )

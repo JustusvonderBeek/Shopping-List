5c3c7fbcae3b3c5be3c435c8af5d282ada4fb3f1
Justus von der Beek <justusvonderbeek@gmail.com>
2024-07-22 16:17:52 +0200

    migrate to new user table

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/ShoppingListDatabase.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/database/ShoppingListDatabase.kt
@@ -4,37 +4,45 @@ import android.content.Context
 import android.util.Log
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteColumn
+import androidx.room.RenameColumn
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.cloudsheeptech.shoppinglist.data.Item
 import com.cloudsheeptech.shoppinglist.data.ListMapping
 import com.cloudsheeptech.shoppinglist.data.ListShareDatabase
 import com.cloudsheeptech.shoppinglist.data.ShoppingList
-import com.cloudsheeptech.shoppinglist.data.DatabaseUser
 import com.cloudsheeptech.shoppinglist.data.ListCreator
 import com.cloudsheeptech.shoppinglist.data.UIPreference
-import com.cloudsheeptech.shoppinglist.data.UserWire
+import com.cloudsheeptech.shoppinglist.data.user.AppUser
+import com.cloudsheeptech.shoppinglist.data.user.AppUserDao
 
 @Database(
-    version = 19,
-    entities = [ShoppingList::class, Item::class, ListMapping::class, DatabaseUser::class, ListCreator::class, ListShareDatabase::class, UIPreference::class],
+    version = 20,
+    entities = [ShoppingList::class, Item::class, ListMapping::class, AppUser::class, ListCreator::class, ListShareDatabase::class, UIPreference::class],
     exportSchema = true,
     autoMigrations = [
-        AutoMigration(from = 18, to = 19)
-    ]
+        AutoMigration(from = 18, to = 19),
+        AutoMigration(from = 19, to = 20, ShoppingListDatabase.Database19To20Migration::class),
+    ],
 )
 @TypeConverters(value = [DatabaseTypeConverter::class])
 abstract class ShoppingListDatabase : RoomDatabase() {
+
     abstract fun shoppingListDao() : ShoppingListDao
     abstract fun itemDao() : ItemDao
     abstract fun mappingDao() : ItemListMappingDao
-    abstract fun userDao() : UserDao
+    abstract fun userDao() : AppUserDao
     abstract fun onlineUserDao() : OnlineUserDao
     abstract fun sharedDao() : SharedDao
     abstract fun preferenceDao() : UIPreferencesDao
 
     companion object {
+        const val LATEST_VERSION = 20
+
         @Volatile
         private  var INSTANCE : ShoppingListDatabase? = null
 
@@ -48,4 +56,19 @@ abstract class ShoppingListDatabase : RoomDatabase() {
             return instance
         }
     }
+
+    @RenameColumn.Entries(
+        RenameColumn(
+            tableName = "list_table",
+            fromColumnName = "CreatedBy",
+            toColumnName = "CreatedByID"
+        ),
+        RenameColumn(
+            tableName = "user",
+            fromColumnName = "UserId",
+            toColumnName = "OnlineID"
+        )
+    )
+    class Database19To20Migration : AutoMigrationSpec
 }
+

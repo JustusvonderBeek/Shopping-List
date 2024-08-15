62353cd9554ad0eb87515182a6ed3643a68c4b82
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 16:51:09 +0200

    changed to data type

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListItemAdapter.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListItemAdapter.kt
@@ -4,17 +4,16 @@ import android.content.res.Resources
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.cloudsheeptech.shoppinglist.data.ItemWithQuantity
-import com.cloudsheeptech.shoppinglist.data.database.ItemListMappingDao
+import com.cloudsheeptech.shoppinglist.data.items.AppItem
+import com.cloudsheeptech.shoppinglist.data.itemToListMapping.ItemListMappingDao
 import com.cloudsheeptech.shoppinglist.databinding.ShoppingItemBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val checkboxClickListener: ShoppingItemCheckboxClickListener, private val resource : Resources, private val mappingDao: ItemListMappingDao, private val listId : Long) : ListAdapter<ItemWithQuantity, ShoppingListItemAdapter.WordListItemViewHolder>(
+class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val checkboxClickListener: ShoppingItemCheckboxClickListener, private val resource : Resources, private val mappingDao: ItemListMappingDao, private val listId : Long) : ListAdapter<AppItem, ShoppingListItemAdapter.WordListItemViewHolder>(
     WordDiffCallback()
 ) {
 
@@ -23,7 +22,7 @@ class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val 
         withContext(Dispatchers.IO) {
             val item = currentList[position]
             // TODO: Replace with list handler
-            mappingDao.deleteMappingItemListId(item.ID, listId)
+            mappingDao.deleteMappingItemListId(item.id, listId, item.addedBy)
         }
         notifyItemRemoved(position)
     }
@@ -37,14 +36,14 @@ class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val 
     }
 
     class WordListItemViewHolder private constructor(val binding : ShoppingItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(clickListener: ShoppingItemClickListener, checkClickListener : ShoppingItemCheckboxClickListener, item : ItemWithQuantity, resource: Resources) {
+        fun bind(clickListener: ShoppingItemClickListener, checkClickListener : ShoppingItemCheckboxClickListener, item : AppItem, resource: Resources) {
             binding.item = item
             binding.clickListener = clickListener
             binding.checkClickListener = checkClickListener
             // When pressing the checkbox itself also update
             binding.itemCheckbox.setOnCheckedChangeListener { _, checked ->
 //                Log.d("ShoppingListItemAdapter", "Checkbox itself pressed")
-                if (item.Checked != checked)
+                if (item.checked != checked)
                     checkClickListener.onClick(item)
             }
 //            Glide.with(binding.root).load(R.drawable.ic_item).into(binding.itemIcon)
@@ -61,19 +60,19 @@ class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val 
     }
 
     class ShoppingItemClickListener(val clickListener: (wordId: Int, count : Int) -> Unit) {
-        fun onClick(item: ItemWithQuantity, count : Int) = clickListener(item.ID.toInt(), count)
+        fun onClick(item: AppItem, count : Int) = clickListener(item.id.toInt(), count)
     }
 
     class ShoppingItemCheckboxClickListener(val clickListener : (itemId : Int) -> Unit) {
-        fun onClick(item: ItemWithQuantity) = clickListener(item.ID.toInt())
+        fun onClick(item: AppItem) = clickListener(item.id.toInt())
     }
 
-    class WordDiffCallback : DiffUtil.ItemCallback<ItemWithQuantity>() {
-        override fun areItemsTheSame(oldItem: ItemWithQuantity, newItem: ItemWithQuantity): Boolean {
-            return oldItem.ID == newItem.ID && oldItem.Name == newItem.Name && oldItem.IconPath == newItem.IconPath
+    class WordDiffCallback : DiffUtil.ItemCallback<AppItem>() {
+        override fun areItemsTheSame(oldItem: AppItem, newItem: AppItem): Boolean {
+            return oldItem.id == newItem.id && oldItem.name == newItem.name && oldItem.icon == newItem.icon
         }
 
-        override fun areContentsTheSame(oldItem: ItemWithQuantity, newItem: ItemWithQuantity): Boolean {
+        override fun areContentsTheSame(oldItem: AppItem, newItem: AppItem): Boolean {
             return oldItem == newItem
         }
     }

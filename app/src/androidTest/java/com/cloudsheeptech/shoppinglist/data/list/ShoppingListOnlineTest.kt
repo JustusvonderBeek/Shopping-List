323f3ffd5925b88eaed33b9252e6169c7259963c
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 16:51:47 +0200

    add initial test

---
+++ app/src/androidTest/java/com/cloudsheeptech/shoppinglist/data/list/ShoppingListOnlineTest.kt
@@ -0,0 +1,41 @@
+package com.cloudsheeptech.shoppinglist.data.list
+
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ShoppingListOnlineTest {
+
+    @Test
+    fun testCreateList() = runTest {
+
+    }
+
+    @Test
+    fun testGetList() = runTest {
+
+    }
+
+    @Test
+    fun testGetAllLists() = runTest {
+
+    }
+
+    @Test
+    fun testUpdateList() = runTest {
+
+    }
+
+    @Test
+    fun testUpdateSharedList() = runTest {
+
+    }
+
+    @Test
+    fun testDeleteList() = runTest {
+
+    }
+
+}

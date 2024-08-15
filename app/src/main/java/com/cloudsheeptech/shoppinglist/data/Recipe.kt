b851726fa42ab9e82c134815bf28e82226a7eb07
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 16:48:54 +0200

    changed to data type

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/Recipe.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/Recipe.kt
@@ -1,9 +1,11 @@
 package com.cloudsheeptech.shoppinglist.data
 
+import com.cloudsheeptech.shoppinglist.data.items.DbItem
+
 data class Recipe(
     var ID : Long,
     var Name : String,
     var Description : String,
-    var Items : List<Item>,
+    var dbItems : List<DbItem>,
     var Quantities : List<Float>,
 )

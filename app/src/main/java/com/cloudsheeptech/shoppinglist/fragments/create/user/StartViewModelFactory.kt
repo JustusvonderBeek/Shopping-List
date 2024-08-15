dc751398f8541f54002275b831ee4ba53257f1d4
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 16:55:10 +0200

    update inject type

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/create/user/StartViewModelFactory.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/create/user/StartViewModelFactory.kt
@@ -3,11 +3,12 @@ package com.cloudsheeptech.shoppinglist.fragments.create.user
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.cloudsheeptech.shoppinglist.data.user.AppUserRepository
 
-class StartViewModelFactory(val application: Application) : ViewModelProvider.Factory {
+class StartViewModelFactory(private val userRepository: AppUserRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(StartViewModel::class.java)) {
-            return StartViewModel(application) as T
+            return StartViewModel(userRepository) as T
         }
         throw IllegalArgumentException("Unknown class")
     }

33c953f387d1b1bc5889abaeae852148ef58689e
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 22:30:54 +0200

    add custom equals

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/list/ApiShoppingList.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/list/ApiShoppingList.kt
@@ -6,7 +6,9 @@ import kotlinx.serialization.Contextual
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
+import java.time.Duration
 import java.time.OffsetDateTime
+import kotlin.math.abs
 
 @Serializable
 data class ApiShoppingList @OptIn(ExperimentalSerializationApi::class) constructor(
@@ -24,4 +26,19 @@ data class ApiShoppingList @OptIn(ExperimentalSerializationApi::class) construct
     var lastUpdated : OffsetDateTime,
     @JsonNames("items")
     var items : MutableList<ApiItem>
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        val timeThreshhold = Duration.ofSeconds(3)
+        if (other is ApiShoppingList) {
+            return other.listId == this.listId &&
+                    other.title == this.title &&
+                    other.createdBy.onlineId == this.createdBy.onlineId &&
+                    Duration.ofNanos(abs(other.lastUpdated.toEpochSecond() - this.lastUpdated.toEpochSecond())) < timeThreshhold &&
+                    other.items.size == this.items.size &&
+                    // Make more efficient for longer lists... O(n*m)
+                    this.items.all { item -> null != other.items.firstOrNull { oItem -> item.name == oItem.name } }
+        }
+        return false
+    }
+}

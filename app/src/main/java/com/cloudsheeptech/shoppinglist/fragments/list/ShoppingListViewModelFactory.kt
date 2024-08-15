416196eb4835f2f18bb1e92a1b738bc1c195e1df
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 16:54:43 +0200

    remove unused import

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListViewModelFactory.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListViewModelFactory.kt
@@ -2,10 +2,7 @@ package com.cloudsheeptech.shoppinglist.fragments.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.cloudsheeptech.shoppinglist.data.Item
-import com.cloudsheeptech.shoppinglist.data.ShoppingList
 import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
-import com.cloudsheeptech.shoppinglist.datastructures.ItemListWithName
 
 class ShoppingListViewModelFactory(val database : ShoppingListDatabase, val shoppingListId : Long, val creatorId : Long) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {

e25d7ac72bda12dc9bacfabc25826fbc996c498b
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 11:16:18 +0200

    add global dependency object

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/ShoppingListApplication.kt
@@ -0,0 +1,39 @@
+package com.cloudsheeptech.shoppinglist
+
+import android.app.Application
+import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
+import com.cloudsheeptech.shoppinglist.data.items.ItemLocalDataSource
+import com.cloudsheeptech.shoppinglist.data.items.ItemRepository
+import com.cloudsheeptech.shoppinglist.data.list.ShoppingListLocalDataSource
+import com.cloudsheeptech.shoppinglist.data.list.ShoppingListRemoteDataSource
+import com.cloudsheeptech.shoppinglist.data.list.ShoppingListRepository
+import com.cloudsheeptech.shoppinglist.data.user.AppUserLocalDataSource
+import com.cloudsheeptech.shoppinglist.data.user.AppUserRemoteDataSource
+import com.cloudsheeptech.shoppinglist.data.user.AppUserRepository
+import com.cloudsheeptech.shoppinglist.network.Networking
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+/**
+ * This class is used to create and inject dependencies throughout the application
+ * so that no more manual labor is necessary to handle dependencies.
+ * More information: https://developer.android.com/training/dependency-injection/hilt-android
+ */
+@HiltAndroidApp
+class ShoppingListApplication : Application() {
+
+    // For these injections to work, the constructor needs an @Inject as well
+    @Inject lateinit var appUserLocalDataSource: AppUserLocalDataSource
+    @Inject lateinit var appUserRemoteDataSource: AppUserRemoteDataSource   // Fix the injection of the string in networking
+    @Inject lateinit var appUserRepository: AppUserRepository
+
+    @Inject lateinit var database: ShoppingListDatabase
+    @Inject lateinit var networking: Networking
+    @Inject lateinit var shoppingListLocalDataSource: ShoppingListLocalDataSource
+    @Inject lateinit var shoppingListRemoteDataSource: ShoppingListRemoteDataSource
+
+    @Inject lateinit var itemLocalDataSource: ItemLocalDataSource
+    @Inject lateinit var itemRepository: ItemRepository
+
+
+}

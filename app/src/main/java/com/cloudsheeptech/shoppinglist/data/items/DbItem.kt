d3a0b12a70044802c182131e350c36bf650bdf65
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-15 16:43:05 +0200

    custom equals and hash

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/items/DbItem.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/items/DbItem.kt
@@ -11,11 +11,27 @@ data class DbItem(
     // Can be null or "" in case no icon is specified
     var icon: String
 ) : Comparable<DbItem> {
+
+    override fun equals(other: Any?): Boolean {
+        if (other is DbItem) {
+            // We don't care about the ID, we only care if the same sequence
+            // of characters is found in both
+            return this.name.trim().lowercase() == other.name.trim().lowercase()
+        }
+        return false
+    }
+
     override fun compareTo(other: DbItem): Int {
         if (other.id == this.id)
             return 0
         if (other.id > this.id)
             return -1
         return 1
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        return result
     }
 }

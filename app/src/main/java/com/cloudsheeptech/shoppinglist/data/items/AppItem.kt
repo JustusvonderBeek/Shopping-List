66de48e979f2278d992d5ad5791200505a2985d4
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-17 16:17:45 +0200

    fix description

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/items/AppItem.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/items/AppItem.kt
@@ -1,7 +1,7 @@
 package com.cloudsheeptech.shoppinglist.data.items
 
 data class AppItem(
-    var id: Long,   // Do we need this ID? And what for? Otherwise, can be merged with the ApiItem
+    var id: Long, // We need this ID locally, because of actions like toggle the checkbox etc.
     var name: String,
     var icon: String,
     var quantity : Long,

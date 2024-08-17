c1bd14be00ddc282eb8af48b5861ef20a0a8f9c3
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-17 16:21:24 +0200

    comment unused

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListViewModelFactory.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListViewModelFactory.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
 
 class ShoppingListViewModelFactory(val database : ShoppingListDatabase, val shoppingListId : Long, val creatorId : Long) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(ShoppinglistViewModel::class.java)) {
-            return ShoppinglistViewModel(database, shoppingListId, creatorId) as T
-        }
-        throw IllegalArgumentException("Unknown class")
-    }
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(ShoppinglistViewModel::class.java)) {
+//            return ShoppinglistViewModel(database, shoppingListId, creatorId) as T
+//        }
+//        throw IllegalArgumentException("Unknown class")
+//    }
 
 }

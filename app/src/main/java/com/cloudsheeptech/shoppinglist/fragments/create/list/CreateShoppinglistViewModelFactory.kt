ab8cc8e67e399ba8e0ef473bbc043b7f6aab7073
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-17 16:19:31 +0200

    update constructor

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/create/list/CreateShoppinglistViewModelFactory.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/create/list/CreateShoppinglistViewModelFactory.kt
@@ -3,11 +3,12 @@ package com.cloudsheeptech.shoppinglist.fragments.create.list
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.cloudsheeptech.shoppinglist.data.list.ShoppingListRepository
 
-class CreateShoppinglistViewModelFactory(val application: Application) : ViewModelProvider.Factory {
+class CreateShoppinglistViewModelFactory(val shoppingListRepository: ShoppingListRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CreateShoppinglistViewModel::class.java)) {
-            return CreateShoppinglistViewModel(application) as T
+            return CreateShoppinglistViewModel(shoppingListRepository) as T
         }
         throw IllegalArgumentException("Unknown class")
     }

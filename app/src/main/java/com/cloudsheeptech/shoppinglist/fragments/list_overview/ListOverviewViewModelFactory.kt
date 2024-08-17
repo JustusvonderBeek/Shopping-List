b9f659b63965412967841fc33809a92f05637505
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-17 16:20:24 +0200

    update constructor

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list_overview/ListOverviewViewModelFactory.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list_overview/ListOverviewViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.cloudsheeptech.shoppinglist.fragments.list_overview
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.cloudsheeptech.shoppinglist.data.list.ShoppingListRepository
+import com.cloudsheeptech.shoppinglist.data.user.AppUserRepository
 import com.cloudsheeptech.shoppinglist.fragments.create.user.StartViewModel
 
-class ListOverviewViewModelFactory(val application: Application) : ViewModelProvider.Factory {
+class ListOverviewViewModelFactory(val application: Application, val listRepo: ShoppingListRepository, val userRepo: AppUserRepository) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(StartViewModel::class.java)) {
-            return ListOverviewViewModel(application) as T
+            return ListOverviewViewModel(listRepo, userRepo) as T
         }
         throw IllegalArgumentException("Unknown class")
     }

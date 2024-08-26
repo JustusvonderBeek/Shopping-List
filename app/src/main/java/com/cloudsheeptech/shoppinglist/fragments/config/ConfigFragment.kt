3510b5b4d4642f3b0c807d63cc0e4c2956886a1c
Justus von der Beek <justusvonderbeek@gmail.com>
2024-08-26 23:11:09 +0200

    add create online button

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/config/ConfigFragment.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/config/ConfigFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.cloudsheeptech.shoppinglist.R
 import com.cloudsheeptech.shoppinglist.databinding.FragmentConfigBinding
+import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,6 +37,14 @@ class ConfigFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_config, container, false)
         binding.viewModel = configViewModel
         binding.lifecycleOwner = this
+
+        viewModel.buttonStatus.observe(viewLifecycleOwner, Observer { enable ->
+            if (enable) {
+                binding.toggleUserButton.background = requireContext().getDrawable(R.color.shopping_green)
+            } else {
+                binding.toggleUserButton.background = requireContext().getDrawable(R.color.shopping_red)
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/config/ConfigViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/config/ConfigViewModel.kt
@@ -1,5 +1,6 @@
 package com.cloudsheeptech.shoppinglist.fragments.config
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,12 +8,20 @@ import androidx.lifecycle.map
 import com.cloudsheeptech.shoppinglist.BuildConfig
 import com.cloudsheeptech.shoppinglist.data.user.AppUserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ConfigViewModel @Inject constructor(
     private val userRepository: AppUserRepository
 ): ViewModel() {
+
+    private val job = Job()
+    private val vmScope = CoroutineScope(Dispatchers.Main + job)
 
     private val _appVersion = MutableLiveData<String>("Version: ${BuildConfig.VERSION_NAME}")
     val appVersion : LiveData<String> get() = _appVersion
@@ -21,4 +30,17 @@ class ConfigViewModel @Inject constructor(
 
     private val user = userRepository.readLive()
     val username : LiveData<String> get() = user.map { x -> x.Username }
+
+    // We want to create the user if not already online, aka. id = 0, otherwise delete
+    private val _buttonStatus = user.map { x -> x.OnlineID == 0L }
+    val buttonStatus : LiveData<Boolean> get() = _buttonStatus
+
+    fun toggleUserOnline() {
+        Log.d("ConfigViewModel", "Toggling the current user status")
+        vmScope.launch {
+            userRepository.createOnline()
+        }
+    }
+
+
 }

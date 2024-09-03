1c8b25a3b1e6cb96a6e4b723edecc0483a6584f2
Justus von der Beek <justusvonderbeek@gmail.com>
2024-09-03 12:19:14 +0200

    include order

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/receipt/ApiDescription.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/receipt/ApiDescription.kt
@@ -1,8 +1,10 @@
 package com.cloudsheeptech.shoppinglist.data.receipt
 
+import androidx.lifecycle.MutableLiveData
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiDescription (
+    var order: Int,
     var step: String,
 )

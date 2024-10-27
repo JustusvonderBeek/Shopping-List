bd52845c5db87aa488cefee978b6ed47123c9068
Justus von der Beek <justusvonderbeek@gmail.com>
2024-10-27 16:56:18 +0100

    feat: combine equal urls

--- app/src/main/java/com/cloudsheeptech/shoppinglist/network/UrlProviderEnum.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/network/UrlProviderEnum.kt
@@ -1,10 +1,12 @@
 package com.cloudsheeptech.shoppinglist.network
 
 enum class UrlProviderEnum(
-    val value: String,
+    val url: String,
 ) {
     BASE_URL("https://10.0.2.2:46152"),
-    USER_CREATE("/v1/users"),
-    USER_LOGIN("/v1/users"),
+
+//    BASE_URL("https://cloudsheeptech.com:46152"),
+//    BASE_URL("https://ec2-3-120-40-62.eu-central-1.compute.amazonaws.com:46152/"),
+    BASE_USER_URL("/v1/users"),
     SHOPPING_LIST_CREATE("/v1/list"),
 }

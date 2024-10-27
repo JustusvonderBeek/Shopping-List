c6e5c8b9c1f09ad0cb658d6c549bf64acd8f9b48
Justus von der Beek <justusvonderbeek@gmail.com>
2024-10-27 20:09:56 +0100

    feat: start test util data source initialization

--- app/src/androidTest/java/com/cloudsheeptech/shoppinglist/testUtil/TestUtil.kt
+++ app/src/androidTest/java/com/cloudsheeptech/shoppinglist/testUtil/TestUtil.kt
@@ -1,9 +1,56 @@
 package com.cloudsheeptech.shoppinglist.testUtil
 
-class TestUtil {
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import com.cloudsheeptech.shoppinglist.ShoppingListApplication
+import com.cloudsheeptech.shoppinglist.data.database.ShoppingListDatabase
+import com.cloudsheeptech.shoppinglist.data.user.AppUserLocalDataSource
+import com.cloudsheeptech.shoppinglist.data.user.AppUserRemoteDataSource
+import com.cloudsheeptech.shoppinglist.data.user.UserCreationDataProvider
+import com.cloudsheeptech.shoppinglist.network.Networking
+import com.cloudsheeptech.shoppinglist.network.TokenProvider
 
-    public fun createUserOnline() {
+object TestUtil {
+    var shoppingListApplication: ShoppingListApplication = ShoppingListApplication()
 
+    fun initialize() {
+        createDatabase()
+        createLocalAppUserDS()
+        createNetworking()
+        createRemoteAppUserDS()
     }
 
+    private fun createDatabase(): ShoppingListDatabase {
+        val application = ApplicationProvider.getApplicationContext<Application>()
+        val database = ShoppingListDatabase.getInstance(application)
+        shoppingListApplication.database = database
+        return database
+    }
+
+    private fun createLocalAppUserDS(): AppUserLocalDataSource {
+        val database = createDatabase()
+        val localDataSource = AppUserLocalDataSource(database)
+        shoppingListApplication.appUserLocalDataSource = localDataSource
+        return localDataSource
+    }
+
+    private fun createNetworking(): Networking {
+        val localUserDS = createLocalAppUserDS()
+        val tokenFile = ApplicationProvider.getApplicationContext<Application>().filesDir.path + "/token.txt"
+
+        val userDataPayloadProvider = UserCreationDataProvider(localUserDS)
+        val tokenProvider = TokenProvider(userDataPayloadProvider)
+        val networking = Networking(tokenFile, tokenProvider)
+        shoppingListApplication.userCreationPayloadProvider = userDataPayloadProvider
+        shoppingListApplication.tokenProvider = tokenProvider
+        shoppingListApplication.networking = networking
+        return networking
+    }
+
+    private fun createRemoteAppUserDS(): AppUserRemoteDataSource {
+        val networking = createNetworking()
+        val remoteDataSource = AppUserRemoteDataSource(networking)
+        shoppingListApplication.appUserRemoteDataSource = remoteDataSource
+        return remoteDataSource
+    }
 }

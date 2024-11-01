127091acaa6c855c86f8ab60a19c19b789f733ec
Justus von der Beek <justusvonderbeek@gmail.com>
2024-11-01 21:51:36 +0100

    fix: cleanup networking class and add patch function

--- app/src/main/java/com/cloudsheeptech/shoppinglist/network/Networking.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/network/Networking.kt
@@ -1,15 +1,16 @@
 package com.cloudsheeptech.shoppinglist.network
 
 import android.util.Log
+import com.cloudsheeptech.shoppinglist.data.ApiObjectWithOnlineId
 import com.cloudsheeptech.shoppinglist.exception.UserAuthenticationFailedException
 import com.cloudsheeptech.shoppinglist.exception.UserNotAuthenticatedException
-import com.cloudsheeptech.shoppinglist.exception.UserNotCreatedException
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
+import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
@@ -17,9 +18,11 @@ import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.net.ConnectException
 import java.time.Duration
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.reflect.full.memberProperties
 
 /*
 * This class captures the authentication logic of the application,
@@ -29,7 +32,6 @@ import javax.inject.Singleton
 class Networking
     @Inject
     constructor(
-        private val tokenFile: String,
         private val tokenProvider: TokenProvider,
     ) {
         private val authClient =
@@ -54,6 +56,28 @@ class Networking
                 }
             }
 
+        /**
+         * This function allows to update all objects fed into the post or delete functions
+         * to contain the new user onlineId in case a user has to be created
+         */
+        private fun updateOnlineId(
+            item: Any,
+            newOnlineId: Long,
+        ) {
+            if (item is ApiObjectWithOnlineId) {
+                item.onlineId = newOnlineId
+            }
+            // The idea is to recursively search for members that are ApiObject interface
+            // and change the contained onlineId to the new one
+            // This is using reflection
+            item::class.memberProperties.forEach { property ->
+                val nestedItem = property.call(item)
+                if (nestedItem is ApiObjectWithOnlineId) {
+                    nestedItem.onlineId = newOnlineId
+                }
+            }
+        }
+
         suspend fun GET(
             requestUrlPath: String,
             responseHandler: suspend (response: HttpResponse) -> Unit,
@@ -66,43 +90,13 @@ class Networking
                         throw UserAuthenticationFailedException("user authentication for GET request failed")
                     }
                     responseHandler(response)
-                } catch (ex: Exception) {
-                    /* TODO: Check which errors this method might throw and remove this too general
-                    error handling */
+                } catch (ex: ConnectException) {
                     Log.e("Networking", "Failed to send GET request to $finalRequestUrl")
                 }
             }
         }
 
         suspend fun POST(
-            requestUrlPath: String,
-            data: String,
-            responseHandler: suspend (HttpResponse) -> Unit,
-        ) {
-            POST(requestUrlPath, data, responseHandler, null)
-        }
-
-        @Throws(IllegalAccessError::class)
-        suspend fun POST(
-            requestUrlPath: String,
-            data: String,
-            responseHandler: suspend (HttpResponse) -> Unit,
-            contentUpdater: (suspend (String) -> String)?,
-        ) {
-            withContext(Dispatchers.IO) {
-                try {
-                    var dataToPost = data
-                    if (contentUpdater != null) {
-                        dataToPost = contentUpdater.invoke(data)
-                    }
-                    post(requestUrlPath, data, responseHandler)
-                } catch (ex: Exception) {
-                    Log.w("Networking", "Failed to send POST request to $requestUrlPath: $ex")
-                }
-            }
-        }
-
-        private suspend fun post(
             requestUrlPath: String,
             data: String,
             responseHandler: suspend (HttpResponse) -> Unit,
@@ -115,31 +109,19 @@ class Networking
                             setBody(data)
                         }
                     if (response.status == HttpStatusCode.Unauthorized) {
-                        throw IllegalAccessException("user not authenticated online")
+                        throw UserNotAuthenticatedException("user not authenticated online")
                     }
                     responseHandler(response)
-                } catch (ex: Exception) {
-                    Log.e("Networking", "Failed to send POST to $finalRequestUrl")
+                } catch (ex: ConnectException) {
+                    Log.e("Networking", "failed to send POST request to $finalRequestUrl")
                 }
             }
         }
 
-        @Throws(UserNotAuthenticatedException::class, UserAuthenticationFailedException::class, UserNotCreatedException::class)
+        @Throws(
+            UserNotAuthenticatedException::class,
+        )
         suspend fun PUT(
-            requestUrlPath: String,
-            data: String,
-            responseHandler: suspend (HttpResponse) -> Unit,
-        ) {
-            withContext(Dispatchers.IO) {
-                try {
-                    put(requestUrlPath, data, responseHandler)
-                } catch (ex: Exception) {
-                    Log.w("Networking", "Failed to send POST request to $requestUrlPath: $ex")
-                }
-            }
-        }
-
-        private suspend fun put(
             requestUrlPath: String,
             data: String,
             responseHandler: suspend (HttpResponse) -> Unit,
@@ -152,11 +134,33 @@ class Networking
                             setBody(data)
                         }
                     if (response.status == HttpStatusCode.Unauthorized) {
-                        throw IllegalAccessException("user not authenticated online")
+                        throw UserNotAuthenticatedException("user not authenticated online")
                     }
                     responseHandler(response)
-                } catch (ex: Exception) {
-                    Log.w("Networking", "Failed to send POST request to $finalRequestUrl: $ex")
+                } catch (ex: ConnectException) {
+                    Log.w("Networking", "Failed to send PUT request to $finalRequestUrl: $ex")
+                }
+            }
+        }
+
+        suspend fun PATCH(
+            requestUrlPath: String,
+            data: String,
+            responseHandler: suspend (HttpResponse) -> Unit,
+        ) {
+            withContext(Dispatchers.IO) {
+                val finalRequestUrl = "${UrlProviderEnum.BASE_URL.url}$requestUrlPath"
+                try {
+                    val response: HttpResponse =
+                        authClient.patch(finalRequestUrl) {
+                            setBody(data)
+                        }
+                    if (response.status == HttpStatusCode.Unauthorized) {
+                        throw UserNotAuthenticatedException("user not authenticated online")
+                    }
+                    responseHandler(response)
+                } catch (ex: ConnectException) {
+                    Log.w("Networking", "Failed to send PATH request to $finalRequestUrl: $ex")
                 }
             }
         }
@@ -173,7 +177,7 @@ class Networking
                         throw IllegalAccessException("user not authenticated online")
                     }
                     responseHandler(response)
-                } catch (ex: Exception) {
+                } catch (ex: ConnectException) {
                     Log.w("Networking", "Failed to send DELETE request to $finalRequestUrl: $ex")
                 }
             }

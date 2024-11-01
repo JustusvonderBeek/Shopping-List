759f4346962ca3b66d7048782b5393ca6e7f18fc
Justus von der Beek <justusvonderbeek@gmail.com>
2024-11-01 14:43:47 +0100

    fix: formatting and add comment

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/items/AppItem.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/items/AppItem.kt
@@ -1,10 +1,11 @@
 package com.cloudsheeptech.shoppinglist.data.items
 
 data class AppItem(
-    var id: Long, // We need this ID locally, because of actions like toggle the checkbox etc.
+    // We need this ID locally, because of actions like toggle the checkbox etc.
+    var id: Long, // But do we really? Shouldn't this be possible with just the name?
     var name: String,
     var icon: String,
-    var quantity : Long,
-    var checked : Boolean,
-    var addedBy : Long
+    var quantity: Long,
+    var checked: Boolean,
+    var addedBy: Long,
 )

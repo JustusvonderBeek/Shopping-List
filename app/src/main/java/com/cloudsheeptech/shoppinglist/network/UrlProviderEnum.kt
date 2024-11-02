d4176f500c0cb2f221ce54ab5687dabdaae5c392
Justus von der Beek <justusvonderbeek@gmail.com>
2024-11-02 12:48:23 +0100

    feat: introduce new url endpoints

--- app/src/main/java/com/cloudsheeptech/shoppinglist/network/UrlProviderEnum.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/network/UrlProviderEnum.kt
@@ -5,9 +5,10 @@ enum class UrlProviderEnum(
 ) {
     BASE_URL("https://10.0.2.2:46152"),
 
-//    BASE_URL("https://cloudsheeptech.com:46152"),
+    //    BASE_URL("https://cloudsheeptech.com:46152"),
 //    BASE_URL("https://ec2-3-120-40-62.eu-central-1.compute.amazonaws.com:46152/"),
     BASE_USER_URL("/v1/users"),
-    SHOPPING_LIST_CREATE("/v1/list"),
+    BASE_SHOPPING_LIST_URL("/v1/lists"),
+    SHOPPING_LIST_READ("/v1/list"),
     ITEM_PRICE("/v1/item/prices"),
 }

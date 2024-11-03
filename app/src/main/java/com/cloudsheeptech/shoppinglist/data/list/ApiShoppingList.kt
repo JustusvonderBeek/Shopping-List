58871641b488034dcc1ced125bda690e3bfa7488
Justus von der Beek <justusvonderbeek@gmail.com>
2024-11-03 15:23:31 +0100

    feat: add version field

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/list/ApiShoppingList.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/list/ApiShoppingList.kt
@@ -6,9 +6,7 @@ import kotlinx.serialization.Contextual
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
-import java.time.Duration
 import java.time.OffsetDateTime
-import kotlin.math.abs
 
 @Serializable
 data class ApiShoppingList
@@ -28,19 +26,24 @@ data class ApiShoppingList
         var lastUpdated: OffsetDateTime,
         @JsonNames("items")
         var items: MutableList<ApiItem>,
+        @JsonNames("version")
+        var version: Long,
     ) {
         override fun equals(other: Any?): Boolean {
-            val timeThreshhold = Duration.ofSeconds(3)
-            if (other is ApiShoppingList) {
-                return other.listId == this.listId &&
-                    other.title == this.title &&
-                    other.createdBy.onlineId == this.createdBy.onlineId &&
-                    Duration.ofNanos(abs(other.lastUpdated.toEpochSecond() - this.lastUpdated.toEpochSecond())) < timeThreshhold &&
-                    other.items.size == this.items.size &&
-                    // Make more efficient for longer lists... O(n*m)
-                    this.items.all { item -> null != other.items.firstOrNull { oItem -> item.name == oItem.name } }
-            }
-            return false
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as ApiShoppingList
+
+            if (listId != other.listId) return false
+            if (title != other.title) return false
+            if (createdBy != other.createdBy) return false
+            if (createdAt != other.createdAt) return false
+            if (lastUpdated != other.lastUpdated) return false
+            if (items != other.items) return false
+            if (version != other.version) return false
+
+            return true
         }
 
         override fun hashCode(): Int {
@@ -50,6 +53,7 @@ data class ApiShoppingList
             result = 31 * result + createdAt.hashCode()
             result = 31 * result + lastUpdated.hashCode()
             result = 31 * result + items.hashCode()
+            result = 31 * result + version.hashCode()
             return result
         }
     }

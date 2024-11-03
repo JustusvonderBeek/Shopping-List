24d89534d2118c953a40a5f6a31f07f154d45e86
Justus von der Beek <justusvonderbeek@gmail.com>
2024-11-03 12:24:25 +0100

    fix: move to correct package and formatting

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/list/ShoppingListDao.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/list/ShoppingListDao.kt
@@ -1,4 +1,4 @@
-package com.cloudsheeptech.shoppinglist.data.database
+package com.cloudsheeptech.shoppinglist.data.list
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
@@ -6,51 +6,60 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.cloudsheeptech.shoppinglist.data.list.DbShoppingList
 
 @Dao
 interface ShoppingListDao {
-
     @Insert(onConflict = OnConflictStrategy.ABORT)
-    fun insertList(list : DbShoppingList) : Long
+    fun insertList(list: DbShoppingList): Long
 
     @Update
-    fun updateList(list : DbShoppingList)
+    fun updateList(list: DbShoppingList)
 
     @Query("DELETE FROM list_table WHERE listId = :key AND createdBy = :createdBy")
-    fun deleteList(key : Long, createdBy : Long)
+    fun deleteList(
+        key: Long,
+        createdBy: Long,
+    )
 
     @Query("DELETE FROM list_table")
     fun reset()
 
     @Query("SELECT COUNT(*) FROM list_table")
-    fun getLatestListIdLive() : LiveData<Long>
+    fun getLatestListIdLive(): LiveData<Long>
 
     @Query("SELECT MAX(listId) FROM list_table")
-    fun getLatestListId() : Long
+    fun getLatestListId(): Long
 
     @Query("SELECT MAX(listId) FROM list_table WHERE listId = 0 OR createdBy = :key")
-    fun getLatestOwnListId(key : Long) : Long
+    fun getLatestOwnListId(key: Long): Long
 
     @Query("SELECT MAX(listId) FROM list_table WHERE createdBy = :key")
-    fun getLatestListId(key : Long) : Long
+    fun getLatestListId(key: Long): Long
 
     @Query("SELECT * FROM list_table ORDER BY listId ASC")
-    fun getShoppingListsLive() : LiveData<List<DbShoppingList>>
+    fun getShoppingListsLive(): LiveData<List<DbShoppingList>>
 
     @Query("SELECT * FROM list_table ORDER BY listId ASC")
-    fun getShoppingLists() : List<DbShoppingList>
+    fun getShoppingLists(): List<DbShoppingList>
 
     @Query("SELECT * FROM list_table WHERE listId = :key AND createdBy = :keyCreatedBy")
-    fun getShoppingListLive(key : Long, keyCreatedBy : Long) : LiveData<DbShoppingList>
+    fun getShoppingListLive(
+        key: Long,
+        keyCreatedBy: Long,
+    ): LiveData<DbShoppingList>
 
     @Query("SELECT * FROM list_table WHERE listId = :key AND createdBy = :keyCreatedBy")
-    fun getShoppingList(key : Long, keyCreatedBy: Long) : DbShoppingList?
+    fun getShoppingList(
+        key: Long,
+        keyCreatedBy: Long,
+    ): DbShoppingList?
 
     @Query("SELECT * FROM list_table WHERE createdBy = :createdBy")
-    fun getOwnShoppingLists(createdBy: Long) : List<DbShoppingList>
+    fun getOwnShoppingLists(createdBy: Long): List<DbShoppingList>
 
     @Query("SELECT EXISTS (SELECT * FROM list_table WHERE listId = :key AND createdBy = :keyCreatedBy)")
-    fun exists(key: Long, keyCreatedBy: Long) : Boolean
-
+    fun exists(
+        key: Long,
+        keyCreatedBy: Long,
+    ): Boolean
 }

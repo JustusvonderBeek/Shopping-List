3b0c11b41f80a5b963260fa253625fae11fa9edb
Justus von der Beek <justusvonderbeek@gmail.com>
2024-12-08 15:24:21 +0100

    fix: formatting

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/itemToListMapping/ItemListMappingDao.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/itemToListMapping/ItemListMappingDao.kt
@@ -9,50 +9,79 @@ import androidx.room.Update
 
 @Dao
 interface ItemListMappingDao {
-
     @Insert(onConflict = OnConflictStrategy.ABORT)
-    fun insertMapping(listMapping: ListMapping) : Long
+    fun insertMapping(listMapping: ListMapping): Long
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
     fun updateMapping(listMapping: ListMapping)
 
     @Query("DELETE FROM item_to_list_mapping WHERE ID = :key")
-    fun deleteMapping(key : Long)
+    fun deleteMapping(key: Long)
 
     @Query("DELETE FROM item_to_list_mapping WHERE ItemID = :itemId AND ListID = :listId AND CreatedBy = :createdBy")
-    fun deleteMappingItemListId(itemId : Long, listId : Long, createdBy: Long)
+    fun deleteMappingItemListId(
+        itemId: Long,
+        listId: Long,
+        createdBy: Long,
+    )
 
     @Query("DELETE FROM item_to_list_mapping WHERE ListID = :listId AND CreatedBy = :createdBy")
-    fun deleteMappingsForListId(listId : Long, createdBy: Long)
+    fun deleteMappingsForListId(
+        listId: Long,
+        createdBy: Long,
+    )
 
     @Query("DELETE FROM item_to_list_mapping WHERE ListID = :listId AND Checked = 1")
-    fun deleteCheckedMappingsForListId(listId : Long)
+    fun deleteCheckedMappingsForListId(listId: Long)
 
     @Query("DELETE FROM item_to_list_mapping")
     fun clearAll()
 
     @Query("SELECT * FROM item_to_list_mapping WHERE ID = :mappingId")
-    fun getMapping(mappingId : Long) : ListMapping?
+    fun getMapping(mappingId: Long): ListMapping?
 
     @Query("SELECT * FROM item_to_list_mapping WHERE ListID = :listId AND CreatedBy = :createdBy")
-    fun getMappingsForList(listId : Long, createdBy: Long) : List<ListMapping>
+    fun getMappingsForList(
+        listId: Long,
+        createdBy: Long,
+    ): List<ListMapping>
 
     @Query("SELECT * FROM item_to_list_mapping WHERE ListID = :listId AND CreatedBy = :createdBy")
-    fun getMappingsForListLive(listId : Long, createdBy : Long) : LiveData<List<ListMapping>>
+    fun getMappingsForListLive(
+        listId: Long,
+        createdBy: Long,
+    ): LiveData<List<ListMapping>>
 
     @Query("SELECT * FROM item_to_list_mapping WHERE ItemID = :itemId")
-    fun getMappingsForItem(itemId : Long) : List<ListMapping>
+    fun getMappingsForItem(itemId: Long): List<ListMapping>
 
     @Query("SELECT * FROM item_to_list_mapping WHERE ItemID = :itemId")
-    fun getMappingsForItemLive(itemId : Long) : LiveData<List<ListMapping>>
+    fun getMappingsForItemLive(itemId: Long): LiveData<List<ListMapping>>
 
     @Query("SELECT * FROM item_to_list_mapping WHERE ItemID = :itemId AND ListID = :listId AND CreatedBy = :createdBy")
-    fun getMappingForItemAndList(itemId : Long, listId : Long, createdBy: Long) : List<ListMapping> // FIXME: Why a list?
+    fun getMappingForItemAndList(itemId: Long, listId: Long, createdBy: Long): List<ListMapping> // FIXME: Why a list?
 
-    @Query("SELECT CASE " +
+    @Query(
+        "SELECT CASE " +
             "WHEN (SELECT COUNT(*) FROM item_to_list_mapping WHERE ListID = :listId AND CreatedBy = :createdBy AND Checked = 1) > 0 " +
             "THEN (SELECT COUNT(distinct Checked) FROM item_to_list_mapping WHERE ListID = :listId AND CreatedBy = :createdBy) " +
             "ELSE 0 " +
-            "END AS count_distinct_values")
-    fun getIsListFinishedLive(listId : Long, createdBy: Long) : LiveData<Int>
+            "END AS count_distinct_values",
+    )
+    fun getIsListFinishedLive(
+        listId: Long,
+        createdBy: Long,
+    ): LiveData<Int>
+
+    @Query("UPDATE item_to_list_mapping SET CreatedBy = 0 WHERE CreatedBy = :createdBy")
+    fun resetCreatorIdForItemsInOwnLists(createdBy: Long)
+
+    @Query("UPDATE item_to_list_mapping SET AddedBy = 0 WHERE AddedBy = :addedBy")
+    fun resetAddedByIdForItemsInOwnLists(addedBy: Long)
+
+    @Query("UPDATE item_to_list_mapping SET CreatedBy = :createdBy WHERE CreatedBy = 0")
+    fun setCreatorIdForAllItems(createdBy: Long)
+
+    @Query("UPDATE item_to_list_mapping SET AddedBy = :addedBy WHERE AddedBy = 0")
+    fun setAddedByIdForItemsInOwnLists(addedBy: Long)
 }

a931ed1fbbbf4ff9b29cbe661d75749bbe38fa26
Justus von der Beek <justusvonderbeek@gmail.com>
2024-12-15 12:36:25 +0100

    fix: refactor

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListItemAdapter.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/list/ShoppingListItemAdapter.kt
@@ -7,17 +7,23 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.cloudsheeptech.shoppinglist.data.items.AppItem
 import com.cloudsheeptech.shoppinglist.data.itemToListMapping.ItemListMappingDao
+import com.cloudsheeptech.shoppinglist.data.items.AppItem
 import com.cloudsheeptech.shoppinglist.databinding.ShoppingItemBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val checkboxClickListener: ShoppingItemCheckboxClickListener, private val resource : Resources, private val mappingDao: ItemListMappingDao, private val listId : Long) : ListAdapter<AppItem, ShoppingListItemAdapter.WordListItemViewHolder>(
+class ShoppingListItemAdapter(
+    val clickListener: ShoppingItemClickListener,
+    val checkboxClickListener: ShoppingItemCheckboxClickListener,
+    private val resource: Resources,
+    private val mappingDao: ItemListMappingDao,
+    private val listId: Long
+) : ListAdapter<AppItem, ShoppingListItemAdapter.WordListItemViewHolder>(
     WordDiffCallback()
 ) {
 
-    suspend fun deleteItemAt(position : Int) {
+    suspend fun deleteItemAt(position: Int) {
         Log.i("WordListItemAdapter", "Remove item at $position")
         withContext(Dispatchers.IO) {
             val item = currentList[position]
@@ -32,11 +38,17 @@ class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val 
     }
 
     override fun onBindViewHolder(holder: WordListItemViewHolder, position: Int) {
-        holder.bind(clickListener, checkboxClickListener, getItem(position),  resource)
+        holder.bind(clickListener, checkboxClickListener, getItem(position), resource)
     }
 
-    class WordListItemViewHolder private constructor(val binding : ShoppingItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(clickListener: ShoppingItemClickListener, checkClickListener : ShoppingItemCheckboxClickListener, item : AppItem, resource: Resources) {
+    class WordListItemViewHolder private constructor(val binding: ShoppingItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            clickListener: ShoppingItemClickListener,
+            checkClickListener: ShoppingItemCheckboxClickListener,
+            item: AppItem,
+            resource: Resources
+        ) {
             binding.item = item
             binding.clickListener = clickListener
             binding.checkClickListener = checkClickListener
@@ -51,7 +63,7 @@ class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val 
         }
 
         companion object {
-            fun from(parent : ViewGroup) : WordListItemViewHolder {
+            fun from(parent: ViewGroup): WordListItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ShoppingItemBinding.inflate(layoutInflater, parent, false)
                 return WordListItemViewHolder(binding)
@@ -59,11 +71,11 @@ class ShoppingListItemAdapter(val clickListener: ShoppingItemClickListener, val 
         }
     }
 
-    class ShoppingItemClickListener(val clickListener: (wordId: Int, count : Int) -> Unit) {
-        fun onClick(item: AppItem, count : Int) = clickListener(item.id.toInt(), count)
+    class ShoppingItemClickListener(val clickListener: (wordId: Int, count: Int) -> Unit) {
+        fun onClick(item: AppItem, count: Int) = clickListener(item.id.toInt(), count)
     }
 
-    class ShoppingItemCheckboxClickListener(val clickListener : (itemId : Int) -> Unit) {
+    class ShoppingItemCheckboxClickListener(val clickListener: (itemId: Int) -> Unit) {
         fun onClick(item: AppItem) = clickListener(item.id.toInt())
     }
 

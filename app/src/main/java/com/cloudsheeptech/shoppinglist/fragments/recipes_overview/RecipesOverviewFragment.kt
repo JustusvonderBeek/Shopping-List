e3d4362bafb437dd5178686e1fa78251e4ff72a3
Justus von der Beek <justusvonderbeek@gmail.com>
2024-12-27 15:41:06 +0100

    fix: not loading items in recipe

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/recipes_overview/RecipesOverviewFragment.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/recipes_overview/RecipesOverviewFragment.kt
@@ -1,9 +1,7 @@
 package com.cloudsheeptech.shoppinglist.fragments.recipes_overview
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -12,6 +10,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -22,8 +22,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class RecipesOverviewFragment : Fragment(), MenuProvider {
 
-    private lateinit var binding : FragmentReceiptsOverviewBinding
-    private val viewModel : RecipesOverviewViewModel by viewModels()
+    private lateinit var binding: FragmentReceiptsOverviewBinding
+    private val viewModel: RecipesOverviewViewModel by viewModels()
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         menuInflater.inflate(R.menu.receipts_drop_down_menu, menu)
@@ -39,24 +39,22 @@ class RecipesOverviewFragment : Fragment(), MenuProvider {
         return false
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_receipts_overview, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_receipts_overview, container, false)
 
         requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-        val adapter = RecipesListAdapter(RecipesListAdapter.ReceiptClickListener { id, from ->
-            Log.d("ReceiptsOverviewFragment", "Got receipt $id from $from")
-            viewModel.navigateToReceipt(id, from)
-        }, requireActivity().resources)
+        val adapter =
+            RecipesListAdapter(RecipesListAdapter.ReceiptClickListener { id, from, title ->
+                Log.d("ReceiptsOverviewFragment", "Got receipt $id from $from")
+                viewModel.navigateToReceipt(id, from, title)
+            }, requireActivity().resources)
         binding.receiptOverviewList.adapter = adapter
 
         viewModel.receipts.observe(viewLifecycleOwner, Observer { list ->
@@ -67,16 +65,28 @@ class RecipesOverviewFragment : Fragment(), MenuProvider {
 
         viewModel.navigateToCreateReceipt.observe(viewLifecycleOwner, Observer { navigate ->
             if (navigate) {
-                findNavController().navigate(RecipesOverviewFragmentDirections.actionReceiptsOverviewToReceiptEditFragment(-1, -1))
+                findNavController().navigate(
+                    RecipesOverviewFragmentDirections.actionReceiptsOverviewToReceiptEditFragment(
+                        -1,
+                        -1
+                    )
+                )
                 viewModel.onCreateReceiptNavigate()
             }
         })
 
-        viewModel.navigateToReceipt.observe(viewLifecycleOwner, Observer { idAndfrom ->
-            val id = idAndfrom.first
-            val from = idAndfrom.second
+        viewModel.navigateToReceipt.observe(viewLifecycleOwner, Observer { idAndfromAndTitle ->
+            val id = idAndfromAndTitle.first
+            val from = idAndfromAndTitle.second
+            val title = idAndfromAndTitle.third
             if (id > 0L) {
-                findNavController().navigate(RecipesOverviewFragmentDirections.actionReceiptsOverviewToReceipts(id, from))
+                findNavController().navigate(
+                    RecipesOverviewFragmentDirections.actionReceiptsOverviewToReceipts(
+                        receiptId = id,
+                        createdBy = from,
+                        title = title
+                    )
+                )
                 viewModel.onReceiptNavigated()
             }
         })

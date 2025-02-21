687d170fd2473387ec3e872050491c18f743a2cc
Justus von der Beek <justusvonderbeek@gmail.com>
2025-02-21 12:42:11 +0100

    wip: try using retrofit

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/network/ShoppingListAPI.kt
@@ -0,0 +1,20 @@
+package com.cloudsheeptech.shoppinglist.network
+
+import com.cloudsheeptech.shoppinglist.data.list.ApiShoppingList
+import com.cloudsheeptech.shoppinglist.data.recipe.ApiRecipe
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ShoppingListAPI {
+    @GET("/v1/list/{id}")
+    suspend fun get(id: Long): ApiShoppingList
+
+    @Multipart
+    @POST("v1/recipe")
+    suspend fun createRecipe(
+        @Part("object") recipe: ApiRecipe,
+        @Part("content") images: Array<Byte>,
+    )
+}

9fa96ce73b9024dd092fcc0a5b725c583c7dc145
Justus von der Beek <justusvonderbeek@gmail.com>
2025-02-21 12:41:48 +0100

    feat: add certprovider for retrofit

---
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/recipe/CertificateProvider.kt
@@ -0,0 +1,21 @@
+package com.cloudsheeptech.shoppinglist.data.recipe
+
+import android.content.Context
+import com.cloudsheeptech.shoppinglist.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import java.io.InputStream
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CertificateProvider {
+    @Provides
+    @Singleton
+    fun provideAppCertificate(
+        @ApplicationContext context: Context,
+    ): InputStream = context.resources.openRawResource(R.raw.shoppinglist_local)
+}

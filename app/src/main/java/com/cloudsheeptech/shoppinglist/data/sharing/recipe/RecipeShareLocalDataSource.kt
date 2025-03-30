53ceb7b58a703cd72537102253c632f8402b6efe
Justus von der Beek <justusvonderbeek@gmail.com>
2025-03-30 18:36:34 +0200

    fix: add annotation for serialization

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/sharing/recipe/RecipeShareLocalDataSource.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/sharing/recipe/RecipeShareLocalDataSource.kt
@@ -6,54 +6,72 @@ import com.cloudsheeptech.shoppinglist.data.recipe.RecipeRepository
 import com.cloudsheeptech.shoppinglist.data.user.AppUserRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.InternalSerializationApi
 import javax.inject.Inject
 
-class RecipeShareLocalDataSource @Inject constructor(
-    database: ShoppingListDatabase,
-    private val userRepo: AppUserRepository,
-    private val recipeRepo: RecipeRepository
-) {
+class RecipeShareLocalDataSource
+    @Inject
+    constructor(
+        database: ShoppingListDatabase,
+        private val userRepo: AppUserRepository,
+        private val recipeRepo: RecipeRepository,
+    ) {
+        private val shareDao = database.recipeShareDao()
 
-    private val shareDao = database.recipeShareDao()
-
-    suspend fun create(recipeId: Long, createdBy: Long, sharedWith: Long): RecipeShare {
-        var createdSharedWith: RecipeShare
-        withContext(Dispatchers.IO) {
-            val user = userRepo.read() ?: throw IllegalStateException("user not initialized")
-            val recipe = recipeRepo.read(recipeId, createdBy)
-                ?: throw IllegalArgumentException("recipe does not exist")
-            // TODO: Check if the user we share with is contained in our local store
-            // Consequence might be an error on the server, but not too relevant
-            val newShare = RecipeShare(recipeId, createdBy, sharedWith)
-            createdSharedWith = newShare
-            shareDao.insert(newShare)
+        @OptIn(InternalSerializationApi::class)
+        suspend fun create(
+            recipeId: Long,
+            createdBy: Long,
+            sharedWith: Long,
+        ): RecipeShare {
+            var createdSharedWith: RecipeShare
+            withContext(Dispatchers.IO) {
+                val user = userRepo.read() ?: throw IllegalStateException("user not initialized")
+                val recipe =
+                    recipeRepo.read(recipeId, createdBy)
+                        ?: throw IllegalArgumentException("recipe does not exist")
+                // TODO: Check if the user we share with is contained in our local store
+                // Consequence might be an error on the server, but not too relevant
+                val newShare = RecipeShare(recipeId, createdBy, sharedWith)
+                createdSharedWith = newShare
+                shareDao.insert(newShare)
+            }
+            return createdSharedWith
         }
-        return createdSharedWith
-    }
 
-    suspend fun read(recipeId: Long, createdBy: Long): List<RecipeShare> {
-        val recipeSharedWithUserIds = mutableListOf<RecipeShare>()
-        withContext(Dispatchers.IO) {
-            val sharedWithIds = shareDao.get(recipeId, createdBy)
-            recipeSharedWithUserIds.addAll(sharedWithIds)
+        suspend fun read(
+            recipeId: Long,
+            createdBy: Long,
+        ): List<RecipeShare> {
+            val recipeSharedWithUserIds = mutableListOf<RecipeShare>()
+            withContext(Dispatchers.IO) {
+                val sharedWithIds = shareDao.get(recipeId, createdBy)
+                recipeSharedWithUserIds.addAll(sharedWithIds)
+            }
+            return recipeSharedWithUserIds
         }
-        return recipeSharedWithUserIds
-    }
 
-    fun readLive(recipeId: Long, createdBy: Long): LiveData<List<RecipeShare>> {
-        return shareDao.getLive(recipeId, createdBy)
-    }
+        fun readLive(
+            recipeId: Long,
+            createdBy: Long,
+        ): LiveData<List<RecipeShare>> = shareDao.getLive(recipeId, createdBy)
 
-    suspend fun delete(recipeId: Long, createdBy: Long, sharedWith: Long) {
-        withContext(Dispatchers.IO) {
-            shareDao.delete(RecipeShare(recipeId, createdBy, sharedWith))
+        suspend fun delete(
+            recipeId: Long,
+            createdBy: Long,
+            sharedWith: Long,
+        ) {
+            withContext(Dispatchers.IO) {
+                shareDao.delete(RecipeShare(recipeId, createdBy, sharedWith))
+            }
+        }
+
+        suspend fun deleteAll(
+            recipeId: Long,
+            createdBy: Long,
+        ) {
+            withContext(Dispatchers.IO) {
+                shareDao.deleteAll(recipeId, createdBy)
+            }
         }
     }
-
-    suspend fun deleteAll(recipeId: Long, createdBy: Long) {
-        withContext(Dispatchers.IO) {
-            shareDao.deleteAll(recipeId, createdBy)
-        }
-    }
-
-}

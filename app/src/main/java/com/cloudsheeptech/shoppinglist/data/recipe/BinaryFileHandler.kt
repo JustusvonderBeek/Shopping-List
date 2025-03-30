ec67613dd437c7fa22cd6fe317deabd1e4278015
Justus von der Beek <justusvonderbeek@gmail.com>
2025-03-30 18:34:35 +0200

    feat: adapt parameter type

--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/recipe/BinaryFileHandler.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/recipe/BinaryFileHandler.kt
@@ -3,47 +3,50 @@ package com.cloudsheeptech.shoppinglist.data.recipe
 import android.net.Uri
 import android.util.Log
 import androidx.core.net.toFile
+import androidx.core.net.toUri
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class BinaryFileHandler : IBinaryFileHandler {
-
-    override suspend fun readImageFromFile(imageUri: Uri): ByteArray? {
-        var binaryImage: ByteArray? = null
-        withContext(Dispatchers.IO) {
-            try {
-                if (!imageUri.toFile().exists()) {
-                    Log.e("BinaryFileHandler", "File does not exist")
-                    return@withContext
+@Singleton
+class BinaryFileHandler
+    @Inject
+    constructor() : IBinaryFileHandler {
+        override suspend fun readImageFromFile(imageLocation: String): ByteArray? {
+            var binaryImage: ByteArray? = null
+            withContext(Dispatchers.IO) {
+                try {
+                    val imageUri = imageLocation.toUri()
+                    if (!imageUri.toFile().exists()) {
+                        Log.e("BinaryFileHandler", "File does not exist")
+                        return@withContext
+                    }
+                    binaryImage = imageUri.toFile().readBytes()
+                } catch (ex: IllegalArgumentException) {
+                    Log.e("BinaryFileHandler", "Given URI in wrong format for local file: $ex")
+                } catch (ex: IOException) {
+                    Log.e("BinaryFileHandler", "Failed to read local file: $ex")
+                } catch (ex: Exception) {
+                    Log.e("BinaryFileHandler", "Unknown exception while reading local file: $ex")
                 }
-                binaryImage = imageUri.toFile().readBytes()
-
-            } catch (ex: IllegalArgumentException) {
-                Log.e("BinaryFileHandler", "Given URI in wrong format for local file: $ex")
-            } catch (ex: IOException) {
-                Log.e("BinaryFileHandler", "Failed to read local file: $ex")
-            } catch (ex: Exception) {
-                Log.e("BinaryFileHandler", "Unknown exception while reading local file: $ex")
             }
+            return binaryImage
         }
-        return binaryImage
-    }
 
-    override suspend fun readImagesFromFiles(imageUris: List<Uri>): List<ByteArray> {
-        val binaryImages = mutableListOf<ByteArray>()
-        for (uri in imageUris) {
-            val image = readImageFromFile(uri)
-            if (image != null) {
-                binaryImages.add(image)
+        override suspend fun readImagesFromFiles(imageUris: List<String>): List<ByteArray> {
+            val binaryImages = mutableListOf<ByteArray>()
+            for (uri in imageUris) {
+                val image = readImageFromFile(uri)
+                if (image != null) {
+                    binaryImages.add(image)
+                }
             }
+            return binaryImages
         }
-        return binaryImages
+
+        override suspend fun storeImageToFile(imageByteArray: ByteArray): Uri {
+            TODO("Not yet implemented")
+        }
     }
-
-    override suspend fun storeImageToFile(imageByteArray: ByteArray): Uri {
-        TODO("Not yet implemented")
-    }
-
-
-}
--- app/src/main/java/com/cloudsheeptech/shoppinglist/data/recipe/IBinaryFileHandler.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/data/recipe/IBinaryFileHandler.kt
@@ -3,11 +3,9 @@ package com.cloudsheeptech.shoppinglist.data.recipe
 import android.net.Uri
 
 interface IBinaryFileHandler {
+    suspend fun readImageFromFile(imageUri: String): ByteArray?
 
-    suspend fun readImageFromFile(imageUri: Uri): ByteArray?
-
-    suspend fun readImagesFromFiles(imageUris: List<Uri>): List<ByteArray>
+    suspend fun readImagesFromFiles(imageUris: List<String>): List<ByteArray>
 
     suspend fun storeImageToFile(imageByteArray: ByteArray): Uri
-
 }

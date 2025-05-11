2fce3c3458377dcc5291ecefd1e1331aed21a836
Justus von der Beek <justusvonderbeek@gmail.com>
2025-05-11 14:14:02 +0200

    feat: restyle recipe view

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/recipe/RecipeDescriptionAdapter.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/recipe/RecipeDescriptionAdapter.kt
@@ -5,27 +5,38 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.cloudsheeptech.shoppinglist.R
 import com.cloudsheeptech.shoppinglist.data.recipe.ApiDescription
 import com.cloudsheeptech.shoppinglist.databinding.ReceiptDescriptionBinding
 
-class RecipeDescriptionAdapter() : ListAdapter<ApiDescription, RecipeDescriptionAdapter.DescriptionViewHolder>(ItemDiffCallback())
-{
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DescriptionViewHolder {
-        return DescriptionViewHolder.from(parent)
-    }
+class RecipeDescriptionAdapter : ListAdapter<ApiDescription, RecipeDescriptionAdapter.DescriptionViewHolder>(ItemDiffCallback()) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): DescriptionViewHolder = DescriptionViewHolder.from(parent)
 
-    override fun onBindViewHolder(holder: DescriptionViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: DescriptionViewHolder,
+        position: Int,
+    ) {
         holder.bind(getItem(position))
     }
 
-    class DescriptionViewHolder private constructor(val binding : ReceiptDescriptionBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(description : ApiDescription) {
+    class DescriptionViewHolder private constructor(
+        val binding: ReceiptDescriptionBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(description: ApiDescription) {
             binding.description = description
+            val context = binding.root.context
+            val evenColor = context.resources.getColor(R.color.app_gray_700, context.theme)
+            val unevenColor = context.resources.getColor(R.color.app_gray_900, context.theme)
+            val backgroundColor = if (bindingAdapterPosition % 2 == 0) evenColor else unevenColor
+            binding.descriptionWrapperCard.setCardBackgroundColor(backgroundColor)
             binding.executePendingBindings()
         }
 
         companion object {
-            fun from(parent : ViewGroup) : DescriptionViewHolder {
+            fun from(parent: ViewGroup): DescriptionViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ReceiptDescriptionBinding.inflate(layoutInflater, parent, false)
                 return DescriptionViewHolder(binding)
@@ -34,13 +45,14 @@ class RecipeDescriptionAdapter() : ListAdapter<ApiDescription, RecipeDescription
     }
 
     class ItemDiffCallback : DiffUtil.ItemCallback<ApiDescription>() {
-        override fun areItemsTheSame(oldItem: ApiDescription, newItem: ApiDescription): Boolean {
-            return oldItem.step == newItem.step
-        }
+        override fun areItemsTheSame(
+            oldItem: ApiDescription,
+            newItem: ApiDescription,
+        ): Boolean = oldItem.step == newItem.step
 
-        override fun areContentsTheSame(oldItem: ApiDescription, newItem: ApiDescription): Boolean {
-            return oldItem == newItem
-        }
+        override fun areContentsTheSame(
+            oldItem: ApiDescription,
+            newItem: ApiDescription,
+        ): Boolean = oldItem == newItem
     }
-
 }
--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/recipe/RecipeIngredientAdapter.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/recipe/RecipeIngredientAdapter.kt
@@ -5,27 +5,38 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.cloudsheeptech.shoppinglist.R
 import com.cloudsheeptech.shoppinglist.data.recipe.ApiIngredient
 import com.cloudsheeptech.shoppinglist.databinding.ReceiptItemBinding
 
-class RecipeIngredientAdapter() : ListAdapter<ApiIngredient, RecipeIngredientAdapter.ReceiptItemViewHolder>(ItemDiffCallback()) {
+class RecipeIngredientAdapter : ListAdapter<ApiIngredient, RecipeIngredientAdapter.ReceiptItemViewHolder>(ItemDiffCallback()) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ReceiptItemViewHolder = ReceiptItemViewHolder.from(parent)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReceiptItemViewHolder {
-        return ReceiptItemViewHolder.from(parent)
-    }
-
-    override fun onBindViewHolder(holder: ReceiptItemViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: ReceiptItemViewHolder,
+        position: Int,
+    ) {
         holder.bind(getItem(position))
     }
 
-    class ReceiptItemViewHolder private constructor(val binding : ReceiptItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(ingredient : ApiIngredient) {
+    class ReceiptItemViewHolder private constructor(
+        val binding: ReceiptItemBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(ingredient: ApiIngredient) {
             binding.apiIngredient = ingredient
+            val context = binding.root.context
+            val evenColor = context.resources.getColor(R.color.app_green_700, context.theme)
+            val unevenColor = context.resources.getColor(R.color.app_green_900, context.theme)
+            val backgroundColor = if (bindingAdapterPosition % 2 == 0) evenColor else unevenColor
+            binding.recipeItemCard.setCardBackgroundColor(backgroundColor)
             binding.executePendingBindings()
         }
 
         companion object {
-            fun from(parent : ViewGroup) : ReceiptItemViewHolder {
+            fun from(parent: ViewGroup): ReceiptItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ReceiptItemBinding.inflate(layoutInflater, parent, false)
                 return ReceiptItemViewHolder(binding)
@@ -34,13 +45,14 @@ class RecipeIngredientAdapter() : ListAdapter<ApiIngredient, RecipeIngredientAda
     }
 
     class ItemDiffCallback : DiffUtil.ItemCallback<ApiIngredient>() {
-        override fun areItemsTheSame(oldItem: ApiIngredient, newItem: ApiIngredient): Boolean {
-            return oldItem.name == newItem.name && oldItem.quantity == newItem.quantity && oldItem.quantityType == newItem.quantityType
-        }
+        override fun areItemsTheSame(
+            oldItem: ApiIngredient,
+            newItem: ApiIngredient,
+        ): Boolean = oldItem.name == newItem.name && oldItem.quantity == newItem.quantity && oldItem.quantityType == newItem.quantityType
 
-        override fun areContentsTheSame(oldItem: ApiIngredient, newItem: ApiIngredient): Boolean {
-            return oldItem == newItem
-        }
+        override fun areContentsTheSame(
+            oldItem: ApiIngredient,
+            newItem: ApiIngredient,
+        ): Boolean = oldItem == newItem
     }
-
 }

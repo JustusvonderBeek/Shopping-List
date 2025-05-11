a37b8c4dfa9c0d4a16e08bdafd5d44cf6f114574
Justus von der Beek <justusvonderbeek@gmail.com>
2025-05-11 11:55:27 +0200

    feat: introduce new color scheme

--- app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/config/ConfigFragment.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/fragments/config/ConfigFragment.kt
@@ -1,22 +1,21 @@
 package com.cloudsheeptech.shoppinglist.fragments.config
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.cloudsheeptech.shoppinglist.R
 import com.cloudsheeptech.shoppinglist.databinding.FragmentConfigBinding
-import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ConfigFragment : Fragment() {
-    private lateinit var binding : FragmentConfigBinding
-    private val configViewModel : ConfigViewModel by viewModels()
+    private lateinit var binding: FragmentConfigBinding
+    private val configViewModel: ConfigViewModel by viewModels()
 
     companion object {
         fun newInstance() = ConfigFragment()
@@ -31,22 +30,26 @@ class ConfigFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_config, container, false)
         binding.viewModel = configViewModel
         binding.lifecycleOwner = this
 
-        viewModel.offlineUser.observe(viewLifecycleOwner, Observer { offline ->
-            if (offline) {
-                binding.toggleUserButton.setBackgroundColor(requireContext().getColor(R.color.shopping_green))
-                binding.toggleUserButton.text = requireContext().getText(R.string.config_btn_create_online)
-            } else {
-                binding.toggleUserButton.setBackgroundColor(requireContext().getColor(R.color.shopping_red))
-                binding.toggleUserButton.text = requireContext().getText(R.string.config_btn_delete_online)
-            }
-        })
+        viewModel.offlineUser.observe(
+            viewLifecycleOwner,
+            Observer { offline ->
+                if (offline) {
+                    binding.toggleUserButton.setBackgroundColor(requireContext().getColor(R.color.app_green_0))
+                    binding.toggleUserButton.text = requireContext().getText(R.string.config_btn_create_online)
+                } else {
+                    binding.toggleUserButton.setBackgroundColor(requireContext().getColor(R.color.app_red_0))
+                    binding.toggleUserButton.text = requireContext().getText(R.string.config_btn_delete_online)
+                }
+            },
+        )
 
         return binding.root
     }

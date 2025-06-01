f38773796dc23c610b8c988850382499bb176d14
Justus von der Beek <justusvonderbeek@gmail.com>
2025-06-01 13:11:24 +0200

    feat: switch to correct domain

--- app/src/main/java/com/cloudsheeptech/shoppinglist/network/UrlProviderEnum.kt
+++ app/src/main/java/com/cloudsheeptech/shoppinglist/network/UrlProviderEnum.kt
@@ -3,10 +3,10 @@ package com.cloudsheeptech.shoppinglist.network
 enum class UrlProviderEnum(
     var url: String,
 ) {
-    //    BASE_URL("https://10.0.2.2:46152"),
-    BASE_URL("http://10.0.2.2:46152"),
+    //    BASE_URL("http://10.0.2.2:46152"),
+//    BASE_URL("https://192.168.0.200:46152"),
 
-    //    BASE_URL("https://shop.cloudsheeptech.com:46152"),
+    BASE_URL("https://shop.cloudsheeptech.com:46152"),
     LOGIN("/v1/users/login"),
 
     BASE_USER_URL("/v1/users"),
